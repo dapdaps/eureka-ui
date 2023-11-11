e00081df53d9b70fb83a8d56e4a0f62d37bfc22c
Emily <emily@ref.finance>
2023-11-11 18:10:04 +0800

    Added tokens.

--- src/stores/tokens.ts
+++ src/stores/tokens.ts
@@ -21,7 +21,7 @@ export const useTokensStore = create(
     }),
     {
       name: 'import-tokens',
-      version: 0.8,
+      version: 1,
       storage: createJSONStorage(() => localStorage),
     },
   ),
