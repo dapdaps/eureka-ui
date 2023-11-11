6b33dd294c30087e7d2ed933703ef79e89d08c59
Emily <emily@ref.finance>
2023-11-11 14:42:32 +0800

    Fixed get token amounts.

--- src/views/Uniswap/utils/getPosition.ts
+++ src/views/Uniswap/utils/getPosition.ts
@@ -1,4 +1,5 @@
 import { Contract } from 'ethers';
+import Big from 'big.js';
 import config from '@/config/uniswap/linea/index';
 import positionAbi from '../abi/positionAbi';
 
@@ -17,21 +18,20 @@ export async function getPositionCollect(args: string[], provider: any) {
 }
 
 export function getTokenAmounts({ liquidity, sqrtPriceX96, tickLow, tickHigh, currentTick, Decimal0, Decimal1 }: any) {
-  const sqrtRatioA = Math.sqrt(1.0001 ** tickLow);
-  const sqrtRatioB = Math.sqrt(1.0001 ** tickHigh);
-  const sqrtPrice = sqrtPriceX96 / 2 ** 96;
-  let amount0 = 0;
-  let amount1 = 0;
+  const sqrtRatioA = new Big(1.0001).pow(tickLow);
+  const sqrtRatioB = new Big(1.0001).pow(tickHigh);
+  const sqrtPrice = new Big(sqrtPriceX96 || 1).div(new Big(2).pow(96));
+  let amount0 = new Big(0);
+  let amount1 = new Big(0);
   if (currentTick < tickLow) {
-    amount0 = Math.floor(liquidity * ((sqrtRatioB - sqrtRatioA) / (sqrtRatioA * sqrtRatioB)));
+    amount0 = new Big(liquidity).mul(sqrtRatioB.minus(sqrtRatioA).div(sqrtRatioA.mul(sqrtRatioB)));
   } else if (currentTick >= tickHigh) {
-    amount1 = Math.floor(liquidity * (sqrtRatioB - sqrtRatioA));
+    amount1 = new Big(liquidity).mul(sqrtRatioB.minus(sqrtRatioA));
   } else if (currentTick >= tickLow && currentTick < tickHigh) {
-    amount0 = Math.floor(liquidity * ((sqrtRatioB - sqrtPrice) / (sqrtPrice * sqrtRatioB)));
-    amount1 = Math.floor(liquidity * (sqrtPrice - sqrtRatioA));
+    amount0 = new Big(liquidity).mul(sqrtRatioB.minus(sqrtPrice).div(sqrtPrice.mul(sqrtRatioB)));
+    amount1 = new Big(liquidity).mul(sqrtPrice.minus(sqrtRatioA));
   }
-
-  const amount0Human = (amount0 / 10 ** Decimal0).toFixed(Decimal0);
-  const amount1Human = (amount1 / 10 ** Decimal1).toFixed(Decimal1);
+  const amount0Human = amount0.div(10 ** Decimal0).toFixed(Decimal0);
+  const amount1Human = amount1.div(10 ** Decimal1).toFixed(Decimal1);
   return [amount0Human, amount1Human];
 }
