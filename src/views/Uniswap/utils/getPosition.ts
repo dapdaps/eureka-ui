40a6a2d7e6e14119da7e8e4035449235d94159c8
Emily <emily@ref.finance>
2023-11-22 13:53:23 +0800

    Fixed amounts out.

--- src/views/Uniswap/utils/getPosition.ts
+++ src/views/Uniswap/utils/getPosition.ts
@@ -24,32 +24,54 @@ export async function getTokenAmounts({ liquidity, tickLower, tickUpper, tick, d
     getSqrtRatioAtTick(tickUpper, provider),
     getSqrtRatioAtTick(tick, provider),
   ]);
-  const _current0 = tick < tickLower ? new Big(sqrtRatioLower) : new Big(sqrtRatio);
-  const _reverse0 = new Big(sqrtRatioUpper).gt(_current0);
-  const _sqrtRatio0A = _reverse0 ? new Big(_current0) : new Big(sqrtRatioUpper);
-  const _sqrtRatio0B = _reverse0 ? new Big(sqrtRatioUpper) : new Big(_current0);
-  const _diff0 = _sqrtRatio0B.minus(_sqrtRatio0A);
 
-  const _reverse1 = new Big(sqrtRatioLower).gt(sqrtRatio);
-  const _sqrtRatio1A = _reverse1 ? new Big(sqrtRatio) : new Big(sqrtRatioLower);
-  const _sqrtRatio1B = _reverse1 ? new Big(sqrtRatioLower) : new Big(sqrtRatio);
-  const _diff1 = _sqrtRatio1B.minus(_sqrtRatio1A);
+  let _amount0 = '0';
+  let _amount1 = '0';
 
   const q96 = 2 ** 96;
-  const amount0 = new Big(liquidity)
-    .mul(q96)
-    .mul(_diff0)
-    .div(_sqrtRatio0A)
-    .div(_sqrtRatio0B)
-    .div(10 ** decimal0);
 
-  const amount1 =
-    tick < tickLower
-      ? new Big(0)
-      : new Big(liquidity)
-          .mul(_diff1)
-          .div(q96)
-          .div(10 ** decimal1);
+  function calc_amount0() {
+    if (tick < tickLower || tick < tickUpper) {
+      const _current0 = tick < tickLower ? new Big(sqrtRatioLower) : new Big(sqrtRatio);
+      const _reverse0 = new Big(sqrtRatioUpper).gt(_current0);
+      const _sqrtRatio0A = _reverse0 ? new Big(_current0) : new Big(sqrtRatioUpper);
+      const _sqrtRatio0B = _reverse0 ? new Big(sqrtRatioUpper) : new Big(_current0);
+      const _diff0 = _sqrtRatio0B.minus(_sqrtRatio0A);
+      const amount0 = new Big(liquidity)
+        .mul(q96)
+        .mul(_diff0)
+        .div(_sqrtRatio0A)
+        .div(_sqrtRatio0B)
+        .div(10 ** decimal0);
+      _amount0 = amount0.toFixed(decimal0);
+    } else {
+      _amount0 = '0';
+    }
+  }
 
-  return [amount0.toFixed(decimal0), amount1.toFixed(decimal1)];
+  function calc_amount1() {
+    if (tick < tickLower) {
+      _amount1 = '0';
+    } else {
+      const _current1 = tick < tickUpper ? new Big(sqrtRatio) : new Big(sqrtRatioUpper);
+      const _reverse1 = new Big(sqrtRatioLower).gt(_current1);
+      const _sqrtRatio1A = _reverse1 ? new Big(_current1) : new Big(sqrtRatioLower);
+      const _sqrtRatio1B = _reverse1 ? new Big(sqrtRatioLower) : new Big(_current1);
+      const _diff1 = _sqrtRatio1B.minus(_sqrtRatio1A);
+
+      const amount1 =
+        tick < tickLower
+          ? new Big(0)
+          : new Big(liquidity)
+              .mul(_diff1)
+              .div(q96)
+              .div(10 ** decimal1);
+      _amount1 = amount1.toFixed(decimal1);
+    }
+  }
+
+  calc_amount0();
+  calc_amount1();
+
+  return [_amount0, _amount1];
 }
