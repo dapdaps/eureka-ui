c476e6bbf7bf828c319a9486c0590fcac165eec6
Emily <emily@ref.finance>
2023-12-05 19:04:27 +0800

    Fixed empty icon.

--- src/views/Uniswap/Pools.tsx
+++ src/views/Uniswap/Pools.tsx
@@ -186,7 +186,7 @@ const Pools = () => {
           </Positions>
         ) : (
           <Empty>
-            {(!account || !positions.length) && chainId === config.chainId && (
+            {(!account || (!positions.length && chainId === config.chainId)) && (
               <svg xmlns="http://www.w3.org/2000/svg" width="42" height="34" viewBox="0 0 42 34" fill="none">
                 <path
                   d="M1 17L8.08994 3.65422C8.95763 2.02092 10.656 1 12.5055 1H29.4945C31.344 1 33.0424 2.02093 33.9101 3.65422L41 17M1 17V29C1 31.2091 2.79086 33 5 33H37C39.2091 33 41 31.2091 41 29V17M1 17H13L17 23H25L28.5 17H41"
