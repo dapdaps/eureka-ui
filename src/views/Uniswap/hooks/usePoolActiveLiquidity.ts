82ee658b59735765be841c1cdda9031150943114
Emily <emily@ref.finance>
2023-12-13 18:32:25 +0800

    Fixed 5703.

--- src/views/Uniswap/hooks/usePoolActiveLiquidity.ts
+++ src/views/Uniswap/hooks/usePoolActiveLiquidity.ts
@@ -3,78 +3,87 @@ import JSBI from 'jsbi';
 import usePool from './usePool';
 import useAllV3Ticks from './useAllV3Ticks';
 import { useAddLiquidityStore } from '@/stores/addLiquidity';
-import { TickProcessed, ChartEntry, getActiveTick, PRICE_FIXED_DIGITS, computeSurroundingTicks  } from '../utils/chartMath';
-import mockData from './mock.json';
-import  { tickToPriceDecimal } from '../utils/chartMath';
-import  { tickToPrice } from '../utils/tickMath';
-import  { getTokenAddress } from '../utils';
+import {
+  TickProcessed,
+  ChartEntry,
+  getActiveTick,
+  PRICE_FIXED_DIGITS,
+  computeSurroundingTicks,
+} from '../utils/chartMath';
+import { tickToPriceDecimal } from '../utils/chartMath';
+import useAccount from '@/hooks/useAccount';
+import config from '@/config/uniswap';
+import { tickToPrice } from '../utils/tickMath';
+import { getTokenAddress } from '../utils';
 // import { FeeAmount, nearestUsableTick, Pool, TICK_SPACINGS, tickToPrice, priceToClosestTick } from '@uniswap/v3-sdk';
 
-export default function usePoolActiveLiquidity(reverse:boolean) {
+export default function usePoolActiveLiquidity(reverse: boolean) {
   const addLiquidityStore: any = useAddLiquidityStore();
+  const { chainId } = useAccount();
   const { token0, token1, fee } = addLiquidityStore;
   // get pool
   const { pool } = usePool(getTokenAddress(token0?.address, true), getTokenAddress(token1?.address, true), fee) as any;
   const { poolAddress, currentTick, liquidity } = pool || {};
-  
+
   // get all v3 ticks
   const { ticks, loading } = useAllV3Ticks(poolAddress) as any;
 
   // get active tick
   const activeTick = useMemo(() => getActiveTick(currentTick, fee), [currentTick, fee]) as any;
-  if (!pool || loading ) return null;
+  if (config.chainId !== chainId) return { current: null };
+  if (!pool || loading) return null;
   const baseData = {
     fee,
     current: tickToPrice({
       tick: currentTick,
-      decimals0: reverse ? token1.decimals: token0.decimals,
-      decimals1: reverse ? token0.decimals: token1.decimals,
+      decimals0: reverse ? token1.decimals : token0.decimals,
+      decimals1: reverse ? token0.decimals : token1.decimals,
       isReverse: !reverse,
       isNumber: true,
-    })
-  }
+    }),
+  };
   if (!+baseData.current) return {};
 
   // get pivot
-  const pivot = ticks.findIndex(({ tick } : any ) => tick > activeTick) - 1;
+  const pivot = ticks.findIndex(({ tick }: any) => tick > activeTick) - 1;
   if (pivot < 0 || !ticks.length) return baseData;
 
-  // process ticks 
+  // process ticks
   const activeTickProcessed: TickProcessed = {
     liquidityActive: JSBI.BigInt(liquidity) ?? 0,
     tick: activeTick,
     liquidityNet: Number(ticks[pivot].tick) === activeTick ? JSBI.BigInt(ticks[pivot].liquidityNet) : JSBI.BigInt(0),
     price0: tickToPrice({
       tick: activeTick,
-      decimals0: reverse ? token1.decimals: token0.decimals,
-      decimals1: reverse ? token0.decimals: token1.decimals,
+      decimals0: reverse ? token1.decimals : token0.decimals,
+      decimals1: reverse ? token0.decimals : token1.decimals,
       isReverse: !reverse,
       isNumber: true,
     }),
-  }
-  const subsequentTicks = computeSurroundingTicks(token0, token1, activeTickProcessed, ticks, pivot, true, reverse)
-  const previousTicks = computeSurroundingTicks(token0, token1, activeTickProcessed, ticks, pivot, false, reverse)
-  const ticksProcessed = previousTicks.concat(activeTickProcessed).concat(subsequentTicks)
+  };
+  const subsequentTicks = computeSurroundingTicks(token0, token1, activeTickProcessed, ticks, pivot, true, reverse);
+  const previousTicks = computeSurroundingTicks(token0, token1, activeTickProcessed, ticks, pivot, false, reverse);
+  const ticksProcessed = previousTicks.concat(activeTickProcessed).concat(subsequentTicks);
   const formattedData = formatData(ticksProcessed);
-  return { data:formattedData, ...baseData };
+  return { data: formattedData, ...baseData };
 }
 function formatData(data: TickProcessed[]) {
   if (!data?.length) {
-    return undefined
+    return undefined;
   }
 
-  const newData: ChartEntry[] = []
+  const newData: ChartEntry[] = [];
   for (let i = 0; i < data.length; i++) {
     const t: TickProcessed = data[i];
     const chartEntry = {
       activeLiquidity: parseFloat(t.liquidityActive.toString()),
       price0: parseFloat(t.price0),
-    }
+    };
 
     if (chartEntry.activeLiquidity > 0) {
-      newData.push(chartEntry)
+      newData.push(chartEntry);
     }
   }
 
-  return newData
+  return newData;
 }
