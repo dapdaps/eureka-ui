760629d37bec49d9e2fd59434d04beeb048be2dc
Ethan <starjs@qq.com>
2024-03-25 09:57:23 +0800

    feat: aave v3 config

--- src/config/lending/dapps/aave.ts
+++ src/config/lending/dapps/aave.ts
@@ -16,7 +16,7 @@ const CONTRACT_ABI = {
   walletBalanceProviderABI:
     'https://raw.githubusercontent.com/corndao/aave-v3-bos-app/main/abi/WalletBalanceProvider.json',
 };
-const heroData = ['Net Worth', 'Net APY'];
+const heroData = ['Net Worth', 'Net APY', 'Health Factor'];
 const baseConfig = { ownerId, nodeUrl, ipfsPrefix, heroData };
 const basic = {
   name: 'AAVE V3',
