b8e939dc98d58ba719add631d272c4ea86dd78f9
Emily <emily@ref.finance>
2024-04-22 19:07:03 +0800

    added router for some dapps.

---
+++ src/pages/layer-bank.tsx
@@ -0,0 +1,14 @@
+import { useDefaultLayout } from '@/hooks/useLayout';
+
+import SingleDapp from '@/views/SingleDapp';
+import type { NextPageWithLayout } from '@/utils/types';
+
+// set dynamic routes for dapps in config file
+
+export const Page: NextPageWithLayout = () => {
+  return <SingleDapp dappPathname="layer-bank" />;
+};
+
+Page.getLayout = useDefaultLayout;
+
+export default Page;
