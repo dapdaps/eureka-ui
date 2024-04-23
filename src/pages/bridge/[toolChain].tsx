472f2c8a4409612ca449aa349e6a4e44a27c2742
ben <benjm240@gmail.com>
2024-04-23 11:40:57 +0800

    change xyfinance source

--- src/pages/bridge/[toolChain].tsx
+++ src/pages/bridge/[toolChain].tsx
@@ -187,7 +187,7 @@ const AllInOne: NextPageWithLayout = () => {
   const prices = usePriceStore((store) => store.price);
   const [tab, setTab] = useState('');
   const cachedTabsStore: any = useAllInOneTabCachedStore();
-  const { addAction } = useAddAction('XYFinance');
+  const { addAction } = useAddAction('dapp');
 
 
   useEffect(() => {
