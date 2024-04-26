2dc5b2fc8edfb656308bc58bbfc3cac4b5f39dfb
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2024-04-26 16:31:54 +0800

    feat: add blastoff entry

--- src/config/dapp/liquidity.ts
+++ src/config/dapp/liquidity.ts
@@ -6,13 +6,13 @@ const ROUTE_CONFIG = {
       '--button-text-color': '#1E2028',
     },
   },
-  // 'arrakis-finance': {
-  //   type: 'liquidity',
-  //   theme: {
-  //     '--button-color': '#FFA760',
-  //     '--button-text-color': '#000000',
-  //   },
-  // },
+  'blastoff': {
+    type: 'liquidity',
+    theme: {
+      '--button-color': '#F49102',
+      '--button-text-color': '#000',
+    }
+  }
 };
 
 export default ROUTE_CONFIG;
