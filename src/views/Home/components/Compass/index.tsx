5ba1e8b585a499add717314d7cdefe0fd58e727c
Ethan <starjs@qq.com>
2024-04-30 15:28:58 +0800

    feat: odyssey enter

--- src/views/Home/components/Compass/index.tsx
+++ src/views/Home/components/Compass/index.tsx
@@ -1,16 +1,19 @@
+import { useDebounceFn } from 'ahooks';
+import { useRouter } from 'next/router';
+import { memo, useMemo, useRef, useState } from 'react';
+import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 import CompassIcon from '@/components/Icons/Compass';
 import Loading from '@/components/Icons/Loading';
 import WinPtsIcon from '@/components/Icons/WinPts';
-import useToast from '@/hooks/useToast';
-import useAuthCheck from '@/hooks/useAuthCheck';
-import { get } from '@/utils/http';
-import { useRouter } from 'next/router';
-import { memo, useMemo, useRef, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
 import odyssey from '@/config/odyssey';
+import useAuthCheck from '@/hooks/useAuthCheck';
+import useToast from '@/hooks/useToast';
+import { StyledFlex, StyledFont, StyledSvg } from '@/styled/styles';
+import { get } from '@/utils/http';
+
 import useCompassList from './hooks/useCompassList';
-import { useDebounceFn } from 'ahooks';
 import {
   StyledCard,
   StyledCardBackgroundImage,
@@ -18,6 +21,8 @@ import {
   StyledCardDesc,
   StyledCardMainContent,
   StyledCardTitle,
+  StyledChainsImg,
+  StyledCominsoon,
   StyledCompassIcon,
   StyledContainer,
   StyledContent,
@@ -25,15 +30,12 @@ import {
   StyledLoadingWrapper,
   StyledRadialBg,
   StyledRadialBg2,
+  StyledSwiperNextButton,
+  StyledSwiperPrevButton,
+  StyledSwiperWrapper,
   StyledTitle,
   StyledWinPtsIcon,
-  StyledSwiperWrapper,
-  StyledSwiperPrevButton,
-  StyledSwiperNextButton,
-  StyledChainsImg,
-  StyledCominsoon,
 } from './styles';
-import { StyledFlex, StyledFont, StyledSvg } from '@/styled/styles';
 
 const iconRight = (
   <svg xmlns="http://www.w3.org/2000/svg" width="13" height="38" viewBox="0 0 13 38" fill="none">
@@ -86,7 +88,7 @@ const Card = function ({ compass }: any) {
   );
 };
 const Compass = () => {
-  const router = useRouter()
+  const router = useRouter();
   const { loading, compassList } = useCompassList();
   const swiperRef = useRef<any>();
 
@@ -101,19 +103,24 @@ const Compass = () => {
         <StyledRadialBg2 />
 
         <StyledInner>
-          <StyledFlex justifyContent='space-between' style={{ padding: "41px 0 30px" }}>
-            <StyledFont color='#FFF' fontSize='36px' fontWeight='700' lineHeight='165%'>Odyssey</StyledFont>
+          <StyledFlex justifyContent="space-between" style={{ padding: '41px 0 30px' }}>
+            <StyledFont color="#FFF" fontSize="36px" fontWeight="700" lineHeight="165%">
+              Odyssey
+            </StyledFont>
             <StyledFlex
-              gap='8px'
-              style={{ cursor: "pointer" }}
+              gap="8px"
+              style={{ cursor: 'pointer' }}
               onClick={() => {
-                router.push("/odyssey/homepage")
+                router.push('/odyssey/homepage');
               }}
             >
-              <StyledFont color='#EBF479'>Explore all</StyledFont>
+              <StyledFont color="#EBF479">Explore all</StyledFont>
               <StyledSvg>
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" viewBox="0 0 18 12" fill="none">
-                  <path d="M1 5.2C0.558172 5.2 0.2 5.55817 0.2 6C0.2 6.44183 0.558172 6.8 1 6.8L1 5.2ZM17.5657 6.56569C17.8781 6.25327 17.8781 5.74674 17.5657 5.43432L12.4745 0.343147C12.1621 0.0307274 11.6556 0.0307274 11.3431 0.343147C11.0307 0.655566 11.0307 1.1621 11.3431 1.47452L15.8686 6L11.3431 10.5255C11.0307 10.8379 11.0307 11.3444 11.3431 11.6569C11.6556 11.9693 12.1621 11.9693 12.4745 11.6569L17.5657 6.56569ZM1 6.8L17 6.8L17 5.2L1 5.2L1 6.8Z" fill="#EBF479" />
+                  <path
+                    d="M1 5.2C0.558172 5.2 0.2 5.55817 0.2 6C0.2 6.44183 0.558172 6.8 1 6.8L1 5.2ZM17.5657 6.56569C17.8781 6.25327 17.8781 5.74674 17.5657 5.43432L12.4745 0.343147C12.1621 0.0307274 11.6556 0.0307274 11.3431 0.343147C11.0307 0.655566 11.0307 1.1621 11.3431 1.47452L15.8686 6L11.3431 10.5255C11.0307 10.8379 11.0307 11.3444 11.3431 11.6569C11.6556 11.9693 12.1621 11.9693 12.4745 11.6569L17.5657 6.56569ZM1 6.8L17 6.8L17 5.2L1 5.2L1 6.8Z"
+                    fill="#EBF479"
+                  />
                 </svg>
               </StyledSvg>
             </StyledFlex>
@@ -121,12 +128,12 @@ const Compass = () => {
           <StyledSwiperWrapper>
             <Swiper
               width={1244}
-              modules={[Autoplay]}
+              // modules={[Autoplay]}
               slidesPerView={1}
-              autoplay={{ delay: 3000 }}
-              speed={1000}
+              // autoplay={{ delay: 3000 }}
+              // speed={1000}
               spaceBetween={(window.innerWidth - 1244) / 2 + 100}
-              loop
+              // loop
               onSwiper={(swiper) => {
                 swiperRef.current = swiper;
               }}
