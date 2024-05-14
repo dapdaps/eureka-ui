4b2fa64eaa0b70b65b8475721b8ae30668acee68
Emily <emily@ref.finance>
2024-05-14 11:55:42 +0800

    change amount out function for ambient, balancer, mimswap, swapr, thruster finance and trader joe.

--- src/config/swap/dapps/ambient.ts
+++ src/config/swap/dapps/ambient.ts
@@ -4,7 +4,7 @@ import { blast } from '@/config/tokens/blast';
 const basic = {
   name: 'Ambient',
   logo: '/images/apps/ambient.png',
-  amountOutFn: 'bluebiu.near/widget/Swap.Data.AmbientAmountOut',
+  amountOutFn: 'bluebiu.near/widget/Swap.Data.AmountOut',
 };
 const networks = {
   534352: {
@@ -17,8 +17,6 @@ const networks = {
     tokens: [scroll['pxeth'], scroll['usdt'], scroll['eth'], scroll['usdc'], scroll['wbtc']],
   },
   81457: {
-    routerAddress: '0xaAaaaAAAFfe404EE9433EEf0094b6382D81fb958',
-    quoterAddress: '0x6A699AB45ADce02891E6115b81Dfb46CAa5efDb9',
     defaultCurrencies: {
       input: blast['eth'],
       output: blast['usdb'],
