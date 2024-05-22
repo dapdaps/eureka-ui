f8023a5cd2b702401613169ed1bd5e6e9fea20b3
Ethan <starjs@qq.com>
2024-05-23 06:02:22 +0800

    feat: ledgity config

--- src/config/staking/dapps/kelp.ts
+++ src/config/staking/dapps/kelp.ts
@@ -2,55 +2,8 @@ import { linea } from '@/config/tokens/linea';
 
 const basic = {
   name: 'Kelp',
-  data: 'bluebiu.near/widget/Staking.Kelp.Data',
 };
 
-const networks = {
-  // Linea
-  59144: {
-    DepositPool: '0x057297e44a3364139edcf3e1594d6917ed7688c2',
-    StakeTokens: [
-      {
-        ...linea['eth'],
-      },
-      // {
-      //   ...linea['weth'],
-      // },
-    ],
-    ExchangeToken: {
-      ...linea['wrseth'],
-    },
-  },
-  // mode
-  34443: {
-    DepositPool: '0xbDf612E616432AA8e8D7d8cC1A9c934025371c5C',
-    StakeTokens: [
-      {
-        ...linea['eth'],
-      },
-      // {
-      //   ...linea['weth'],
-      // },
-    ],
-    ExchangeToken: {
-      ...linea['wrseth'],
-    },
-  },
-  1: {
-    //TODO
-    DepositPool: '0x036676389e48133B63a802f8635AD39E752D375D',
-    StakeTokens: [
-      {
-        ...linea['eth'],
-      },
-      // {
-      //   ...linea['weth'],
-      // },
-    ],
-    ExchangeToken: {
-      ...linea['wrseth'],
-    },
-  },
-};
+const networks = {};
 
 export default { basic, networks };
