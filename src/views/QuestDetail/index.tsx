d47549e61624b8ab662aec772391167703f72098
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2024-05-29 11:47:00 +0800

    feat: add okx_wallet user

--- src/views/QuestDetail/index.tsx
+++ src/views/QuestDetail/index.tsx
@@ -65,6 +65,7 @@ const QuestDetailView = () => {
 
   const isBitGetUser = useMemo(() => userInfo.source === 'bitget' || userInfo.source === 'bitget_wallet', [userInfo]);
   const isCoin98User = useMemo(() => userInfo.source === 'coin98_wallet', [userInfo]);
+  const isOkxUser = useMemo(() => userInfo.source === 'okx_wallet', [userInfo]);
   useEffect(() => {
     if (wallet?.label.toLowerCase().includes('bitget')) {
       handleReport('bitget_wallet');
@@ -92,6 +93,7 @@ const QuestDetailView = () => {
                 isLive={info.quest.status === 'ongoing'}
                 isBitGetUser={isBitGetUser}
                 isCoin98User={isCoin98User}
+                isOkxUser={isOkxUser}
                 claimed={info.quest.is_claimed}
                 onSuccess={() => {
                   queryUserReward();
@@ -111,6 +113,7 @@ const QuestDetailView = () => {
               reward={info.quest.reward}
               isBitGetUser={isBitGetUser}
               isCoin98User={isCoin98User}
+              isOkxUser={isOkxUser}
               onClose={() => {
                 setShowSuccessModal(false);
               }}
