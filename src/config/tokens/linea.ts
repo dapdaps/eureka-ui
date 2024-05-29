2dcec4cc7fed28b2083865241720e65282ad7bcc
Ethan <starjs@qq.com>
2024-05-30 01:15:52 +0800

    fix: beefy config

--- src/config/tokens/linea.ts
+++ src/config/tokens/linea.ts
@@ -195,4 +195,12 @@ export const linea: { [key: string]: Token } = {
     name: 'Ledgity USDC',
     icon: 'https://assets.coingecko.com/coins/images/6319/small/USD_Coin_icon.png?1547042389',
   },
+  ezeth: {
+    address: '0x2416092f143378750bb29b79ed961ab195cceea5',
+    chainId: CHAIN_ID,
+    symbol: 'ezETH',
+    decimals: 18,
+    name: 'Renzo Restaked ETH',
+    icon: 'https://ipfs.near.social/ipfs/bafkreiee5yhfnbsjedzqj2p4uaeko327ooenqptplslol2pxwir463z7ii',
+  },
 };
