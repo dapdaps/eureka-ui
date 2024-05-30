03901670ece763a7c48a4adf1092d3582ee8ffa0
Emily <emily@ref.finance>
2024-05-30 14:54:22 +0800

    fix build error

--- src/views/AllInOne/Detail.tsx
+++ src/views/AllInOne/Detail.tsx
@@ -14,7 +14,7 @@ import Lending from '@/views/AllInOne/components/Lending';
 import Liquidity from '@/views/AllInOne/components/Liquidity';
 import Trade from '@/views/AllInOne/components/Trade';
 
-import Settings from './components/Setting/index'
+import Settings from './components/Setting/index';
 
 const AllInOneDetailView = (props: Props) => {
   const { chain, menu } = props;
@@ -23,19 +23,14 @@ const AllInOneDetailView = (props: Props) => {
 
   const [showSettings, setShowSettings] = useState<boolean>(false);
 
-  const {
-    currentChain,
-    showComponent,
-    setShowComponent,
-    currentChainMenuList,
-  } = useChain(props);
+  const { currentChain, showComponent, setShowComponent, currentChainMenuList } = useChain(props);
 
   const handleMenuSelect = (tab: string) => {
     router.replace(`/all-in-one/${currentChain.path}/${tab}`);
   };
 
   const currentMenu = useMemo<any>(() => {
-    const defaultMenu = { tab: menu, description: "" };
+    const defaultMenu = { tab: menu, description: '' };
     if (!currentChain) return defaultMenu;
     if (!currentChain.menuConfig) return defaultMenu;
     const currMenu = Object.values(currentChain.menuConfig).find((it: any) => it.tab.toLowerCase() === menu);
@@ -54,11 +49,7 @@ const AllInOneDetailView = (props: Props) => {
   return (
     <>
       <StyledContainer>
-        <StyledFlex
-          flexDirection="column"
-          justifyContent="center"
-          className="all-in-one-wrapper"
-        >
+        <StyledFlex flexDirection="column" justifyContent="center" className="all-in-one-wrapper">
           <AllInOneHeaderView
             chain={chain}
             currentChain={currentChain}
@@ -68,40 +59,22 @@ const AllInOneDetailView = (props: Props) => {
           />
 
           <StyledContent className="detail">
-            {
-              showComponent && (
-                <AllInOneDetailCardView
-                  key={menu}
-                  title={currentMenu?.tab}
-                  subTitle={currentMenu.description}
-                  bgColor={currentChain.selectBgColor}
-                  style={{ width: cardWidth }}
-                  config={cardConfig}
-                  onShowSettings={() => setShowSettings(true)}
-                >
-                  {
-                    menu === 'bridge' && (
-                      <Bridge chain={currentChain} />
-                    )
-                  }
-                  {
-                    menu === 'lending' && (
-                      <Lending chain={currentChain} menu={currentMenu} />
-                    )
-                  }
-                  {
-                    menu === 'liquidity' && (
-                      <Liquidity chain={currentChain} menu={currentMenu} />
-                    )
-                  }
-                  {
-                    menu === 'trade' && (
-                      <Trade chain={currentChain} />
-                    )
-                  }
-                </AllInOneDetailCardView>
-              )
-            }
+            {showComponent && (
+              <AllInOneDetailCardView
+                key={menu}
+                title={currentMenu?.tab}
+                subTitle={currentMenu.description}
+                bgColor={currentChain.selectBgColor}
+                style={{ width: cardWidth }}
+                config={cardConfig}
+                onShowSettings={() => setShowSettings(true)}
+              >
+                {menu === 'bridge' && <Bridge chain={currentChain} />}
+                {menu === 'lending' && <Lending chain={currentChain} menu={currentMenu} />}
+                {menu === 'liquidity' && <Liquidity chain={currentChain} menu={currentMenu} />}
+                {menu === 'trade' && <Trade chain={currentChain} />}
+              </AllInOneDetailCardView>
+            )}
           </StyledContent>
           <StyledNavList>
             {currentChainMenuList.map((item: any) => {
@@ -113,6 +86,7 @@ const AllInOneDetailView = (props: Props) => {
                   subTitle={item.description}
                   bgColor={currentChain.selectBgColor}
                   path={currentChain.path}
+                  chainId={currentChain.chainId}
                   onSelect={() => {
                     const _tab = item.tab.toLowerCase();
                     if (_tab === menu) return;
@@ -126,16 +100,10 @@ const AllInOneDetailView = (props: Props) => {
           </StyledNavList>
         </StyledFlex>
         <StyledBg>
-          <Gradient
-            bgColor={currentChain.selectBgColor}
-            width={720}
-            height={241}
-            rx={280}
-            ry={40.5}
-          />
+          <Gradient bgColor={currentChain.selectBgColor} width={720} height={241} rx={280} ry={40.5} />
         </StyledBg>
       </StyledContainer>
-      <Settings display={showSettings} onClose={() => setShowSettings(false)}/>
+      <Settings display={showSettings} onClose={() => setShowSettings(false)} />
     </>
   );
 };
@@ -146,4 +114,3 @@ interface Props {
   chain: string;
   menu: string;
 }
-
