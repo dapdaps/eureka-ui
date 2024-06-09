a570cb26f91521276f6f68d16d1ab427ba2476d7
ran zhang <zr02718@ly.com>
2024-06-09 09:25:10 +0800

    add hooks

--- src/views/Launchpad/index.tsx
+++ src/views/Launchpad/index.tsx
@@ -1,11 +1,14 @@
 import type { Chain, Token } from '@/types'
 
-import { useQuote, useTrade } from './useFjordTrade'
+import { useBuyQuote,  useSellQuote, useBuyTrade, useSellTrade } from './useFjordTrade'
 import Big from 'big.js'
 import { useState } from 'react'
+import Loading from '@/components/Icons/Loading'
+import useAccount from '@/hooks/useAccount';
 
 const fromChain: Chain = {
     chainId: 42161,
+    // chainId: 1,
     chainName: 'Arbitrum',
     icon: 'https://assets.dapdap.net/images/bafkreiajyg2iof2wygtgromy6a2yfl2fqavfy235k7afc4frr7xnljvu2a.svg',
     nativeCurrency: { name: 'ETH', symbol: 'ETH', decimals: 18 },
@@ -22,16 +25,67 @@ const fromToken: Token = {
     address: '0x0000000000000000000000000000000000000000',
 }
 
+const fromToken2: Token = {
+    chainId: 1,
+    address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    name: 'WETH',
+    symbol: 'WETH',
+    icon: 'https://assets.coingecko.com/coins/images/6319/standard/usdc.png?1696506694',
+    decimals: 18,
+}
+
 export default function BirdgeAction() {
+    const { account, chainId, provider } = useAccount();
+
     const [quote] = useState<any>({
         fromChain: fromChain,
         fromToken: fromToken,
-        amount: new Big('0.01').mul(10 ** 18),
+        amount: new Big('0.001').mul(10 ** 18),
         pool: '0x31BE063F26E5db02Fda4b6B11D037e8Ad8a98988',
         address: '0x86cdCd7fA9F3B24D68CbDD9170C3662036BDC2ef',
     })
 
-    useQuote(quote)
+    const { loading: buyQuoteLoading, bridgeRoute, shareVal, receiveAmount, pool, tradeType, midToken } = useBuyQuote(quote)
 
-    return <div>111</div>
+    const { loading: sellQuoteLoading, assetOut } = useSellQuote({
+        amount: new Big('161.316157182813200429').mul(10 ** 18).toString(),
+        pool: '0x31BE063F26E5db02Fda4b6B11D037e8Ad8a98988',
+        account: '0x86cdCd7fA9F3B24D68CbDD9170C3662036BDC2ef'
+    })
+
+    const { excuteBuyTrade } = useBuyTrade({
+        shareVal,
+        bridgeRoute,
+        receiveAmount,
+        tradeType,
+        pool,
+        midToken,
+        recipient: '0x86cdCd7fA9F3B24D68CbDD9170C3662036BDC2ef',
+        quote,
+    })
+
+    const { excuteSellTrade } = useSellTrade({
+        pool: '0x31BE063F26E5db02Fda4b6B11D037e8Ad8a98988',
+        recipient: '0x86cdCd7fA9F3B24D68CbDD9170C3662036BDC2ef',
+        assetOut,
+        amount: new Big('161.316157182813200429').mul(10 ** 18).toString(),
+    })
+    
+
+    return <div>
+        <div>will buy Val: {shareVal}</div>
+        <button onClick={() => {
+            if (!buyQuoteLoading && shareVal) {
+                excuteBuyTrade(provider.getSigner())
+            }
+        }}> {buyQuoteLoading && <Loading size={20} />} buy trade</button> 
+
+
+        <div>will sell Val: {assetOut}</div>
+        <button onClick={() => {
+            if (!sellQuoteLoading && assetOut) {
+                excuteSellTrade(provider.getSigner())
+            }
+        }}> {sellQuoteLoading && <Loading size={20} />} sell trade</button> 
+    </div>
 }
--- src/views/Launchpad/useFjordTrade.ts
+++ src/views/Launchpad/useFjordTrade.ts
@@ -1,7 +1,10 @@
 import type { Chain, Token } from '@/types'
 import { useEffect, useState } from 'react';
-import { Contract, providers, utils } from 'ethers';
-import { getQuote } from 'super-bridge-sdk'
+import { Contract, providers, utils, Signer } from 'ethers';
+import { getQuote, getStatus, execute, approve } from 'super-bridge-sdk'
+
+import type { QuoteRequest, QuoteResponse } from 'super-bridge-sdk'
+
 
 import tokenConfig from './tokenConfig'
 import Big from 'big.js';
@@ -14,8 +17,25 @@ interface QuoteProps {
     address: string;
 }
 
-interface TradeProps {
+interface QuoteResProps {
+    shareVal: string;
+    loading: boolean;
+    bridgeRoute: QuoteResponse | null;
+    receiveAmount: string;
+    tradeType: number;
+    pool: string;
+    midToken: Token | null;
+}
 
+interface TradeProps {
+    shareVal: string;
+    bridgeRoute: QuoteResponse | null;
+    receiveAmount: string;
+    tradeType: number;
+    pool: string;
+    midToken: Token | null;
+    recipient: string;
+    quote: QuoteProps,
 }
 
 const poolAbi = [
@@ -61,26 +81,297 @@ const poolAbi = [
         "stateMutability": "nonpayable",
         "type": "function"
     },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "assetsIn",
+                "type": "uint256"
+            }
+        ],
+        "name": "previewSharesOut",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "sharesOut",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "sharesIn",
+                "type": "uint256"
+            }
+        ],
+        "name": "previewAssetsOut",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "assetsOut",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "purchasedShares",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "saleStart",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "pure",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "saleEnd",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "pure",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "closed",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "sharesIn",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "minAssetsOut",
+                "type": "uint256"
+            },
+            {
+                "internalType": "address",
+                "name": "recipient",
+                "type": "address"
+            }
+        ],
+        "name": "swapExactSharesForAssets",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "assetsOut",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "recipient",
+                "type": "address"
+            },
+            {
+                "internalType": "bool",
+                "name": "referred",
+                "type": "bool"
+            }
+        ],
+        "name": "redeem",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "shares",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
 ]
 
-export function useQuote(quote: QuoteProps) {
-    const provider = new providers.JsonRpcProvider('https://eth.llamarpc.com');
-    
+const executorAbi = [
+    {
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "_transactionId",
+                "type": "bytes32"
+            },
+            {
+                "components": [
+                    {
+                        "internalType": "address",
+                        "name": "callTo",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "approveTo",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "sendingAssetId",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "receivingAssetId",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "fromAmount",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "bytes",
+                        "name": "callData",
+                        "type": "bytes"
+                    },
+                    {
+                        "internalType": "bool",
+                        "name": "requiresDeposit",
+                        "type": "bool"
+                    }
+                ],
+                "internalType": "struct LibSwap.SwapData[]",
+                "name": "_swapData",
+                "type": "tuple[]"
+            },
+            {
+                "internalType": "address",
+                "name": "_transferredAssetId",
+                "type": "address"
+            },
+            {
+                "internalType": "address payable",
+                "name": "_receiver",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "swapAndExecute",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "erc20Proxy",
+        "outputs": [
+            {
+                "internalType": "contract IERC20Proxy",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+]
+
+export function useBuyQuote(quote: QuoteProps): QuoteResProps {
+    const [loading, setLoading] = useState(false)
+    const [shareVal, setShareVal] = useState('')
+    const [receiveAmount, setReceiveAmount] = useState('')
+    const [bridgeRoute, setBridgeRoute] = useState<QuoteResponse | null>(null)
+    const [tradeType, setTradeType] = useState<number>(1)
+    const [midToken, setMidToken] = useState<Token | null>(null)
 
     useEffect(() => {
-        getFdQuote(quote)
+        if (midToken) {
+            setLoading(true)
+            setShareVal('')
+
+            const provider = new providers.JsonRpcProvider('https://eth.llamarpc.com');
+            const PoolContract = new Contract(
+                quote.pool,
+                poolAbi,
+                provider,
+            )
+
+            try {
+                if (quote.fromChain.chainId === 1 && quote.fromToken.address === midToken.address) {
+                    getAssetRuote(quote, midToken, PoolContract, provider)
+                    setTradeType(1)
+                } else if (quote.fromChain.chainId === 1 && quote.fromToken.address !== midToken.address) {
+                    getBridgeRuote(quote, midToken, PoolContract, provider)
+                    setTradeType(3)
+                } else {
+                    getBridgeRuote(quote, midToken, PoolContract, provider)
+                    setTradeType(3)
+                }
+                
+            } catch (e) {
+                setShareVal('')
+                setBridgeRoute(null)
+                setReceiveAmount('')
+                setLoading(false)
+            }
+        }
+        
+    }, [quote, midToken])
+
+    useEffect(() => {
+        if (quote) {
+            getMidToken(quote.pool)
+        }
     }, [quote])
 
-    async function getFdQuote(quote: QuoteProps) {
-        const RateContract = new Contract(
-            quote.pool,
-            poolAbi,
-            provider,
-        )
-        const middleTokenAddress = await RateContract.asset()
+    async function getBridgeRuote(quote: QuoteProps, midToken: Token, PoolContract: Contract, provider: any) {
+        
+        setBridgeRoute(null)
+        setReceiveAmount('')
 
-        const midToken = tokenConfig[middleTokenAddress]
-
+        
         const bridgeQuote = await getQuote({
             fromChainId: quote.fromChain.chainId.toString(),
             toChainId: '1',
@@ -95,22 +386,258 @@ export function useQuote(quote: QuoteProps) {
 
         console.log('bridgeQuote: ', bridgeQuote)
         if (!bridgeQuote.length) {
-            return null
+            return false
         }
 
         const route = bridgeQuote[0]
 
-        if (route) {
-            const receiveAmount = route.receiveAmount
+        const receiveAmount = route.receiveAmount
 
+        const targetShareVal = await PoolContract.previewSharesOut(receiveAmount)
+
+        console.log('targetShareVal: ', new Big(targetShareVal).div(10 ** 18).toString())
+        setShareVal(new Big(targetShareVal).div(10 ** 18).toString())
+        setBridgeRoute(route)
+        setReceiveAmount(receiveAmount)
+        setLoading(false)
+    }
+
+    async function getAssetRuote(quote: QuoteProps, midToken: Token, PoolContract: Contract, provider: any) {
+        const targetShareVal = await PoolContract.previewSharesOut(quote.amount.toString())
+        console.log('targetShareVal: ', new Big(targetShareVal).div(10 ** 18).toString())
+        setShareVal(new Big(targetShareVal).div(10 ** 18).toString())
+        setReceiveAmount(quote.amount.toString())
+        setLoading(false)
+    }
+
+    async function getMidToken(pool: string) {
+        const PoolContract = getPoolContract(pool)
+
+        const middleTokenAddress = await PoolContract.asset()
+
+        const midToken = tokenConfig[middleTokenAddress]
+        setMidToken(midToken)
+    }
+
+    
+
+    return {
+        shareVal,
+        loading,
+        bridgeRoute,
+        receiveAmount,
+        tradeType,
+        pool: quote.pool,
+        midToken,
+    }
+}
+
+export function useBuyTrade({
+    shareVal,
+    bridgeRoute,
+    receiveAmount,
+    tradeType,
+    pool,
+    midToken,
+    recipient,
+    quote,
+}: TradeProps) {
+
+    async function excuteBuyTrade(signer: Signer) {
+        if (!midToken) {
+            return
+        }
+
+        if (tradeType === 3 && bridgeRoute) { // 1:bridge 2:asset token => target 
+            const bridgeHash = await execute(bridgeRoute, signer)
+            for (let i = 0; i < 999; i++) {
+                const isSuccess = await getStatus(bridgeHash, 'liFi', signer)
+                if (isSuccess) {
+                    break;
+                }
+                sleep(10000)
+            }
+        }
+        
+        if (tradeType === 3 || tradeType === 1) { // 
+            const PoolContract = new Contract(
+                pool,
+                poolAbi,
+                signer,
+            )
+
+            const midTokenBalance = await getBalance('https://eth.llamarpc.com', recipient, midToken)
+
+            let _receiveAmount = receiveAmount
+            if (Number(midTokenBalance) < Number(receiveAmount)) {
+                _receiveAmount = midTokenBalance
+            }    
+
+            const assetsIn = new Big(_receiveAmount)
+            const minSharesOut = new Big(shareVal).mul(10 ** 18).mul(1 - 0.0025).toString()
+
+            await approve(midToken.address, assetsIn, pool, signer)
+
+            const hash = await PoolContract.swapExactAssetsForShares(assetsIn.toString(), minSharesOut.split('.')[0], recipient)
+            
+            console.log('hash: ', hash)
+            return hash
+        }
+
+        if (tradeType === 2) {
+            // const ExecutorContract = new Contract(
+            //     '0x2dfaDAB8266483beD9Fd9A292Ce56596a2D1378D',
+            //     executorAbi,
+            //     signer,
+            // )
+
+            // const apprroveAddress = await ExecutorContract.erc20Proxy()
+
+            // await approve(quote.fromToken.address, quote.amount, apprroveAddress, signer)
         }
     }
 
-
-
-    return {}
+    return {
+        excuteBuyTrade,
+    }
 }
 
-export function useTrade({}: TradeProps) {
+export function useSellQuote({
+    amount, pool, account
+}: {
+    amount: string, pool: string, account: string
+}) {
+    const [assetOut, setAssetOut] = useState('')
+    const [balance, setBalance] = useState('')
+    const [loading, setLoading] = useState(false)
 
+    useEffect(() => {
+        setLoading(true)
+        const PoolContract = getPoolContract(pool)
+        PoolContract.previewAssetsOut(amount).then((res: string) => {
+            // console.log(new Big(res.toString()).div(10 ** 18).toString())
+            setAssetOut(new Big(res.toString()).div(10 ** 18).toString())
+            setLoading(false)
+        })
+
+    }, [amount])
+
+    useEffect(() => {
+        const PoolContract = getPoolContract(pool)
+        PoolContract.purchasedShares(account).then((res: string) => {
+            console.log('balance: ',res.toString())
+            setBalance(new Big(res.toString()).div(10 ** 18).toString())
+        })
+    }, [])
+
+    return {
+        assetOut, loading, balance
+    }
+}
+
+export function useSellTrade({
+    pool,
+    amount,
+    assetOut,
+    recipient,
+}: {
+    pool: string;
+    amount: string;
+    assetOut: string;
+    recipient: string;
+}) {
+    async function excuteSellTrade(signer: Signer) {
+        const PoolContract = new Contract(
+            pool,
+            poolAbi,
+            signer,
+        )
+
+        const minAssetsOut = new Big(assetOut).mul(10 ** 18).mul(1 - 0.0025).toString()
+
+        const hash = await PoolContract.swapExactSharesForAssets(amount,  minAssetsOut.split('.')[0], recipient)
+
+        console.log(hash)
+
+        return hash
+    }
+
+    async function saleStart() {
+        const PoolContract = getPoolContract(pool)
+        const time = await PoolContract.saleStart()
+        console.log('start-time:', time)
+    }
+
+    async function saleEnd() {
+        const PoolContract = getPoolContract(pool)
+        const time = await PoolContract.saleEnd()
+        console.log('end-time:', time)
+    }
+
+    async function getClosed() {
+        const PoolContract = getPoolContract(pool)
+        const time = await PoolContract.closed()
+        console.log('close-time:', time)
+    }
+
+    useEffect(() => {
+        saleStart()
+        saleEnd()
+        getClosed()
+    }, [])
+
+    return {
+        excuteSellTrade
+    }
+}
+
+async function getBalance(rpcUrl: string, account: string, currency: Token) {
+    try {
+        const provider = new providers.JsonRpcProvider(rpcUrl);
+        const TokenContract = new Contract(
+            currency.address,
+            [
+                {
+                    constant: true,
+                    inputs: [
+                        {
+                            name: '_owner',
+                            type: 'address',
+                        },
+                    ],
+                    name: 'balanceOf',
+                    outputs: [
+                        {
+                            name: 'balance',
+                            type: 'uint256',
+                        },
+                    ],
+                    payable: false,
+                    stateMutability: 'view',
+                    type: 'function',
+                },
+            ],
+            provider,
+        );
+        const res = await TokenContract.balanceOf(account);
+        return res.toString()
+    } catch (err) {
+        return '0'
+    }
+};
+
+
+function sleep(time: number){
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function getPoolContract(pool: string) {
+    const provider = new providers.JsonRpcProvider('https://eth.llamarpc.com');
+    const PoolContract = new Contract(
+        pool,
+        poolAbi,
+        provider,
+    )
+
+    return PoolContract
 }
