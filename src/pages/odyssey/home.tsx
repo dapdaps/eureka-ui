3a964b51cec18c52eac65c8e9830b60c2556d43f
Emily <emily@ref.finance>
2024-06-11 21:36:43 +0800

    fix odyssey id uncorrect on production.

--- src/pages/odyssey/home.tsx
+++ src/pages/odyssey/home.tsx
@@ -10,7 +10,11 @@ import OdysseyV8 from '@/views/OdysseyV8';
 
 function Compass() {
   const router = useRouter();
+  if (process.env.NEXT_PUBLIC_API === 'https://api.dapdap.net') {
+    if (router.query.id === '5') return <OdysseyV8 />;
+  }
   if (router.query.id === '8') return <OdysseyV8 />;
+  if (router.query.id === '5') return <OdysseyV5 />;
   if (router.query.id === '4' || router.query.id === '7') return <OdysseyV4 />;
   if (router.query.id === '3') return <OdysseyV3 />;
   if (router.query.id === '2') return <OdysseyV2 />;
