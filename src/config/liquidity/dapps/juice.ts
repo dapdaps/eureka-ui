7345cfd4d7593e25bffa9db2b4e8e0e750ba4615
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2024-06-14 13:09:30 +0800

    fix: modify juice config file

--- src/config/liquidity/dapps/juice.ts
+++ src/config/liquidity/dapps/juice.ts
@@ -11,83 +11,36 @@ const basic = {
     wJUICE: "https://app.juice.finance/images/logos/juice.svg",
     USDB: "https://app.juice.finance/images/logos/tokens/usdb.svg",
     wUSDB: "https://app.juice.finance/images/logos/protocols/wasabi.svg",
-    DUSD: "https://app.juice.finance/images/logos/tokens/dusd.svg"
+    DUSD: "https://app.juice.finance/images/logos/tokens/dusd.svg",
+    DETH: "https://app.juice.finance/images/logos/tokens/deth.svg"
   }
 };
-const USDB_POOLS = {
-  PROXY_ADDRESS: "0xc877b52c628dba77fc55f1ddb140747155c9b39d",
-  LENDING_POOL_ADDRESS: "0x4A1d9220e11a47d8Ab22Ccd82DA616740CF0920a",
-  SYMBOL_ADDRESS: "0x4300000000000000000000000000000000000003",
+const WETH_WETH_POOLS = {
+  PROXY_ADDRESS: "0x23eBa06981B5c2a6f1a985BdCE41BD64D18e6dFA",
+  LENDING_POOL_ADDRESS: "0x44f33bC796f7d3df55040cd3C631628B560715C2",
+  SYMBOL_ADDRESS: "0x4300000000000000000000000000000000000004",
   vaults: [{
-    icon: "	https://app.juice.finance/images/logos/protocols/hyperlock.svg",
-    iconBgColor: "transparent",
-    borderColor: "transparent",
-    name: "USDB/WETH 0.05% LP",
-    type: "Liquidity Pools",
-    protocol: "Hyperlock <> Thruster V3",
-    token0: "USDB",
-    token1: "HYPLP",
-    strategyAddress: "0xd04c891876675f8c02160ee33466315ac13afc38",
-    vaultAddress: "0xd04c891876675f8c02160ee33466315ac13afc38",
-    pointList: [{
-      type: "deposited",
-      label: "Total Deposited",
-    }, {
-      type: "points",
-      label: "Thruster Points",
-    }, {
-      type: "points",
-      label: "Hyperlock Points",
-    }]
-  }, {
     icon: "https://app.juice.finance/images/logos/protocols/particle.svg",
     iconBgColor: "transparent",
     borderColor: "transparent",
-    name: "USDB Boosted Points",
+    name: "WETH Boosted Points",
     type: "Misc",
     protocol: "Particle",
-    token0: "USDB",
-    token1: "DUSD",
-    strategyAddress: "0x542A672B1DEa78EFd83B9D7D8CAe76cEa59964a1",
-    vaultAddress: "0x542A672B1DEa78EFd83B9D7D8CAe76cEa59964a1",
+    token0: "WETH",
+    token1: "DETH",
+    strategyAddress: "0x6F3Bc2f9034C151326A80F5ca1Ee0F1eA1E6f002",
+    vaultAddress: "",
     pointList: [{
       type: "deposited",
       label: "Total Deposited",
     }, {
-      type: "points",
+      type: "multiplier",
       label: "Points Multiplier",
     }, {
       type: "points",
       label: "Particle Points",
     }]
   }, {
-    icon: "	https://app.juice.finance/images/logos/protocols/wasabi.svg",
-    iconBgColor: "transparent",
-    borderColor: "transparent",
-    name: "wUSDB Vault",
-    type: "Perps",
-    protocol: "Wasabi",
-    token0: "USDB",
-    token1: "wUSDB",
-    strategyAddress: "0x0CA56aa647E83A8F0a5f7a81a2fdcA393bC68D78",
-    vaultAddress: "0x0CA56aa647E83A8F0a5f7a81a2fdcA393bC68D78",
-    pointList: [{
-      type: "deposited",
-      label: "Total Deposited",
-    }, {
-      type: "points",
-      label: "Wasabi Points",
-    }, {
-      type: "apy",
-      label: "Variable APY",
-    }]
-  }]
-}
-const WETH_POOLS = {
-  PROXY_ADDRESS: "0x23eBa06981B5c2a6f1a985BdCE41BD64D18e6dFA",
-  LENDING_POOL_ADDRESS: "0x44f33bC796f7d3df55040cd3C631628B560715C2",
-  SYMBOL_ADDRESS: "0x4300000000000000000000000000000000000004",
-  vaults: [{
     icon: "https://app.juice.finance/images/logos/protocols/etherfi.svg",
     iconBgColor: "#FFF",
     name: "EtherFi V3 LP",
@@ -266,14 +219,183 @@ const WETH_POOLS = {
     }]
   }]
 }
+const WETH_USDB_POOLS = {
+  PROXY_ADDRESS: "0x105e285f1a2370d325046fed1424d4e73f6fa2b0",
+  LENDING_POOL_ADDRESS: "0x4A1d9220e11a47d8Ab22Ccd82DA616740CF0920a",
+  SYMBOL_ADDRESS: ["0x4300000000000000000000000000000000000004", "0x4300000000000000000000000000000000000003"],
+  vaults: [{
+    icon: "https://app.juice.finance/images/logos/protocols/ethena.svg",
+    iconBgColor: "transparent",
+    borderColor: "transparent",
+    name: "Ethena USDE V3 LP",
+    type: "Liquidity Pools",
+    protocol: "Hyperlock <> Thruster V3",
+    token0: "USDB",
+    token1: "HYPLP",
+    strategyAddress: "0xc1B1aE2502D2cDEF4772FB4A4a6fcBf4fd9c1b80",
+    vaultAddress: "0x8C1bb76510D6873a4A156A9Cb394E74A3783BDB5",
+    pointList: [{
+      type: "deposited",
+      label: "Total Deposited",
+    }, {
+      type: "sats",
+      label: "Ethena Sats",
+    }, {
+      type: "points",
+      label: "Thruster Points",
+    }, {
+      type: "points",
+      label: "Hyperlock Points",
+    }]
+  }, {
+    icon: "https://app.juice.finance/images/logos/protocols/particle.svg",
+    iconBgColor: "transparent",
+    borderColor: "transparent",
+    name: "USDB Boosted Points",
+    type: "Misc",
+    protocol: "Particle",
+    token0: "USDB",
+    token1: "DUSD",
+    strategyAddress: "0x542A672B1DEa78EFd83B9D7D8CAe76cEa59964a1",
+    vaultAddress: "",
+    pointList: [{
+      type: "deposited",
+      label: "Total Deposited",
+    }, {
+      type: "points",
+      label: "Points Multiplier",
+    }, {
+      type: "points",
+      label: "Particle Points",
+    }]
+  }, {
+    icon: "https://app.juice.finance/images/logos/protocols/thruster.svg",
+    iconBgColor: "transparent",
+    borderColor: "transparent",
+    name: "WETH Spot Long",
+    type: "Spot",
+    protocol: "Thruster V3",
+    token0: "USDB",
+    token1: "WETH",
+    strategyAddress: "0x4A355D57fc1A5eEB33C0a19539744A2144220027",
+    vaultAddress: "0xf00DA13d2960Cf113edCef6e3f30D92E52906537",
+    pointList: [{
+      type: "deposited",
+      label: "Total Deposited",
+    }, {
+      type: "points",
+      label: "Thruster Points",
+    }]
+  }]
+}
+const USDB_USDB_POOLS = {
+  PROXY_ADDRESS: "0xc877b52c628dba77fc55f1ddb140747155c9b39d",
+  LENDING_POOL_ADDRESS: "0x4A1d9220e11a47d8Ab22Ccd82DA616740CF0920a",
+  SYMBOL_ADDRESS: "0x4300000000000000000000000000000000000003",
+  vaults: [{
+    icon: "https://app.juice.finance/images/logos/protocols/ethena.svg",
+    iconBgColor: "transparent",
+    borderColor: "transparent",
+    name: "Ethena USDE V3 LP",
+    type: "Liquidity Pools",
+    protocol: "Hyperlock <> Thruster V3",
+    token0: "USDB",
+    token1: "HYPLP",
+    strategyAddress: "0xc1B1aE2502D2cDEF4772FB4A4a6fcBf4fd9c1b80",
+    vaultAddress: "0x8C1bb76510D6873a4A156A9Cb394E74A3783BDB5",
+    pointList: [{
+      type: "deposited",
+      label: "Total Deposited",
+    }, {
+      type: "sats",
+      label: "Ethena Sats",
+    }, {
+      type: "points",
+      label: "Thruster Points",
+    }, {
+      type: "points",
+      label: "Hyperlock Points",
+    }]
+  }, {
+    icon: "	https://app.juice.finance/images/logos/protocols/hyperlock.svg",
+    iconBgColor: "transparent",
+    borderColor: "transparent",
+    name: "USDB/WETH 0.05% LP",
+    type: "Liquidity Pools",
+    protocol: "Hyperlock <> Thruster V3",
+    token0: "USDB",
+    token1: "HYPLP",
+    strategyAddress: "0xd04c891876675f8c02160ee33466315ac13afc38",
+    vaultAddress: "0x7f0DB0D77d0694F29c3f940b5B1F589FFf6EF2e0",
+    pointList: [{
+      type: "deposited",
+      label: "Total Deposited",
+    }, {
+      type: "points",
+      label: "Thruster Points",
+    }, {
+      type: "points",
+      label: "Hyperlock Points",
+    }]
+  }, {
+    icon: "https://app.juice.finance/images/logos/protocols/particle.svg",
+    iconBgColor: "transparent",
+    borderColor: "transparent",
+    name: "USDB Boosted Points",
+    type: "Misc",
+    protocol: "Particle",
+    token0: "USDB",
+    token1: "DUSD",
+    strategyAddress: "0x542A672B1DEa78EFd83B9D7D8CAe76cEa59964a1",
+    vaultAddress: "",
+    pointList: [{
+      type: "deposited",
+      label: "Total Deposited",
+    }, {
+      type: "points",
+      label: "Points Multiplier",
+    }, {
+      type: "points",
+      label: "Particle Points",
+    }]
+  }, {
+    icon: "	https://app.juice.finance/images/logos/protocols/wasabi.svg",
+    iconBgColor: "transparent",
+    borderColor: "transparent",
+    name: "wUSDB Vault",
+    type: "Perps",
+    protocol: "Wasabi",
+    token0: "USDB",
+    token1: "wUSDB",
+    strategyAddress: "0x0CA56aa647E83A8F0a5f7a81a2fdcA393bC68D78",
+    vaultAddress: "",
+    pointList: [{
+      type: "deposited",
+      label: "Total Deposited",
+    }, {
+      type: "points",
+      label: "Wasabi Points",
+    }, {
+      type: "apy",
+      label: "Variable APY",
+    }]
+  }]
+}
+
 
 const networks = {
   // blast
   81457: {
-    SymbolList: ['USDB Vaults', 'WETH Vaults'],
-    PoolsMapping: {
-      0: USDB_POOLS,
-      1: WETH_POOLS
+    SYMBOL_LIST: ['WETH/WETH', 'WETH/USDB', 'USDB/USDB'],
+    POOLS_MAPPING: {
+      0: WETH_WETH_POOLS,
+      1: WETH_USDB_POOLS,
+      2: USDB_USDB_POOLS,
+    },
+    SYMBOL_NAME_MAPPING: {
+      0: ['WETH', 'WETH'],
+      1: ['WETH', 'USDB'],
+      2: ['USDB', 'USDB'],
     }
   },
 };
