b90bb64f59f8dcb62cae345b6fbd5ed1509dc9bf
Emily <emily@ref.finance>
2024-06-17 14:24:23 +0800

    change the gold amount.

--- src/views/OdysseyV8/components/Spins/quest.tsx
+++ src/views/OdysseyV8/components/Spins/quest.tsx
@@ -19,19 +19,13 @@ import {
   SpinLine,
 } from './styles';
 
-export default function Quest({ data, bgClass, onRefreshDetail, userInfo, authConfig }: any) {
-  const { id, name, step, total_spins, spins } = data;
+export default function Quest({ data, bgClass, userInfo, authConfig }: any) {
+  const { id, name, step } = data;
   const { handleReport } = useReport(true);
-  const [execution, setExecution] = useState(0);
-
-  useEffect(() => {
-    setExecution(total_spins / spins);
-  }, [total_spins, spins]);
 
   const onItemClick = (item: any, i: number) => {
-    if (item.link && item.link?.startsWith('http')) {
+    if (item.link) {
       openLink(item.link, true);
-
       handleReport(id, i + 1);
       return;
     }
@@ -111,45 +105,6 @@ export default function Quest({ data, bgClass, onRefreshDetail, userInfo, authCo
           </SpinLine>
         );
       })}
-      <Body>
-        {/* <div style={{ textAlign: 'center' }}>
-          {isGold ? (
-            <svg width="67" height="67" viewBox="0 0 67 67" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="33.5" cy="33.5" r="33.5" fill="url(#paint0_linear_10_3)" />
-              <path
-                d="M18.3037 27.2651L12.4102 41.1917L32.262 48.6298M18.3037 27.2651L37.6902 17.6113L48.8569 19.0356M18.3037 27.2651L30.0907 30.1137M32.262 48.6298L54.5953 30.1137L48.8569 19.0356M32.262 48.6298L30.0907 30.1137M48.8569 19.0356L30.0907 30.1137"
-                stroke="black"
-                strokeWidth="3"
-                strokeLinejoin="round"
-              />
-              <defs>
-                <linearGradient
-                  id="paint0_linear_10_3"
-                  x1="0"
-                  y1="33.5"
-                  x2="67"
-                  y2="33.5"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stopColor="#FDF621" />
-                  <stop offset="0.595" stopColor="#FDFCC3" />
-                  <stop offset="1" stopColor="#FDF973" />
-                </linearGradient>
-              </defs>
-            </svg>
-          ) : (
-            <Image src={logo} alt="" width={67} height={67} />
-          )}
-        </div>
-        <QuestGoldHints>{rewardHints}</QuestGoldHints>
-        <QuestGold>+{reward}</QuestGold>
-        <div className="quest-desc">{desc}</div>
-        <div className="foot">
-          <TrapeziformBtn width="100%" height="50px" handleClick={handleTrade} loading={reportLoading}>
-            Trade <Image src="/images/odyssey/v4/arrow.svg" alt="" width={23} height={16} />
-          </TrapeziformBtn>
-        </div> */}
-      </Body>
     </Trapeziform>
   );
 }
