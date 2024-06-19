a4cacea62b433a63da89710d851dbaaffefc097e
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2024-06-19 11:29:50 +0800

    feat: add theme color and astar zkevm chain

---
+++ src/config/theme/index.ts
@@ -0,0 +1,21 @@
+
+export default {
+  drak: {
+    "--normal-bg-color": "#262836",
+    "--normal-border-color": "#373A53",
+    "--drak-border-color": "#33364B",
+    "--normal-button-bg-color": "#FFF",
+    "--normal-button-text-color": "#1E2028",
+    "--normal-text-color": "#FFF",
+    "--normal-input-bg-color": "#1B1E27"
+  },
+  light: {
+    "--normal-bg-color": "#FFF",
+    "--normal-border-color": "#38344B",
+    "--drak-border-color": "#000",
+    "--normal-button-bg-color": "#000",
+    "--normal-button-text-color": "#FFF",
+    "--normal-text-color": "#000",
+    "--normal-input-bg-color": "#FFF"
+  }
+}
