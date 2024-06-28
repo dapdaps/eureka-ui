47c616da127776f6017eb5197163524138817e58
Emily <emily@ref.finance>
2024-06-28 15:30:12 +0800

    Convert the user account to lowercase.

--- src/views/Pool/Pools/hooks/usePools.ts
+++ src/views/Pool/Pools/hooks/usePools.ts
@@ -1,22 +1,25 @@
 import { useCallback, useEffect, useState, useRef } from 'react';
 import useAccount from '@/hooks/useAccount';
+import useDappConfig from '../../hooks/useDappConfig';
 
 export default function usePools() {
   const { account } = useAccount();
-
+  const { chainId } = useDappConfig();
   const [pools, setPools] = useState<any>([]);
   const [loading, setLoading] = useState(true);
   const timer = useRef<any>();
 
   const queryList = useCallback(async () => {
-    setLoading(true);
+    if (!account) return;
     setPools([]);
+    setLoading(true);
     try {
       setPools(pools);
       const response = await fetch(
-        `https://api.thruster.finance/v2/user/positions?userAddress=${account}&chainId=81457`,
+        `https://api.thruster.finance/v2/user/positions?userAddress=${account?.toLowerCase()}&chainId=81457`,
       );
       const result = await response.json();
+      setPools(result?.reduce ? result : []);
       setPools(result);
       setLoading(false);
     } catch (err) {
@@ -28,14 +31,14 @@ export default function usePools() {
   useEffect(() => {
     clearTimeout(timer.current);
     timer.current = setTimeout(() => {
-      if (!account) {
+      if (!account || chainId !== 81457) {
         setPools([]);
         setLoading(false);
         return;
       }
       queryList();
     }, 500);
-  }, [account]);
+  }, [account, chainId]);
 
   return { pools, loading };
 }
