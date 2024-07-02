ff9bc4067bbc26b6e4c41084646e06f55d8b7363
ben <benjm240@gmail.com>
2024-07-02 08:27:54 +0800

    change balance floor

--- src/views/SuperBridge/ChainTokenAmount/GasModal.tsx
+++ src/views/SuperBridge/ChainTokenAmount/GasModal.tsx
@@ -4,7 +4,7 @@ import type { QuoteRequest, QuoteResponse, ExecuteRequest } from 'super-bridge-s
 import { useDebounce } from 'ahooks';
 import useAccount from '@/hooks/useAccount';
 import { usePriceStore } from '@/stores/price';
-import { balanceFormated, percentFormated, addressFormated } from '@/utils/balance';
+import { balanceFormated, percentFormated, addressFormated, balanceFormatedFloor } from '@/utils/balance';
 import Loading from '@/components/Icons/Loading';
 import { useGasAmount } from '../hooks/useGasTokenHooks';
 import SubmitBtn from '../SubmitBtn';
@@ -186,7 +186,7 @@ export default function GasModal({
                     <svg width="8" height="10" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M7.5 4.13397C8.16667 4.51887 8.16667 5.48113 7.5 5.86603L1.5 9.33013C0.833334 9.71503 -4.47338e-07 9.2339 -4.13689e-07 8.4641L-1.10848e-07 1.5359C-7.71986e-08 0.766098 0.833333 0.284973 1.5 0.669873L7.5 4.13397Z" fill="#979ABE"/>
                     </svg>
-                <div>{balanceFormated(receive)} {toChain?.nativeCurrency.symbol}</div>
+                <div>{balanceFormatedFloor(receive)} {toChain?.nativeCurrency.symbol}</div>
             </div>
         </RefuelAmount>
 
