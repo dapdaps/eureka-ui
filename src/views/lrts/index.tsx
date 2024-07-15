d939bb0b15aa011076def871245c5a4290e8b4ba
Ethan <starjs@qq.com>
2024-07-15 23:31:35 +0800

    feat: update tab card

--- src/views/lrts/index.tsx
+++ src/views/lrts/index.tsx
@@ -10,18 +10,14 @@ import { usePriceStore } from '@/stores/price';
 import { Gems, NpcDialog, TabCard } from './components';
 import StakeModal from './components/modal/stake';
 import SwapModal from './components/modal/swap';
-import type { CardData } from './components/tab-card';
+import { ActionType } from './components/tab-card';
 import useAllTokensBalance from './hooks/useAllTokensBalance';
 import { Banner, Container, Desc, Title } from './styles/index.style';
 
-enum CardType {
-  LST = 'LST',
-  LRT = 'LRT',
-}
-export enum ActionType {
-  STAKE = 'stake',
-  UNSTAKE = 'unstake',
-}
+// enum CardType {
+//   LST = 'LST',
+//   LRT = 'LRT',
+// }
 
 const Home = () => {
   const { chainId, account } = useAccount();
@@ -32,20 +28,13 @@ const Home = () => {
 
   const [curLrt, setCurLrt] = useState<any>(null);
   const [actionType, setActionType] = useState<ActionType>();
-  const [cardType, setCardType] = useState(CardType.LST);
-  const [cardData, setCardData] = useState<CardData>({
-    tokenName: '',
-    dappName: '',
-    dappLogo: '',
-    apr: 0,
-    tvl: 0,
-    balance: 0,
-  });
+
+  const [cardData, setCardData] = useState();
 
   const [isShowStakeModal, setIsShowStakeModal] = useState(false);
 
   const lrtsData = useLrtDataStore((store: any) => store.data);
-  console.log('lrtsData----', lrtsData);
+  // console.log('lrtsData----', lrtsData);
 
   const { loading, balances } = useAllTokensBalance();
   const [showSwapModal, setShowSwapModal] = useState(false);
@@ -53,33 +42,14 @@ const Home = () => {
   const handleSlideChange = ({ activeIndex }: any) => {
     setIsShowNpc(true);
     setCurrentIndex(activeIndex);
-    setCardType(CardType.LST);
-    const { token, dapp } = lrtsData[activeIndex];
+
     //TODO
-    setCardData({
-      tokenName: token.symbol as string,
-      dappName: dapp.name,
-      dappLogo: dapp.logo,
-      apr: 0,
-      tvl: 0,
-      balance: token?.balance || 0,
-    });
+    setCardData(lrtsData[activeIndex]);
     console.log('click lst', activeIndex, lrtsData[activeIndex]);
   };
 
   const handleClickGem = (lrt: any) => {
     setCurLrt(lrt);
-    setCardType(CardType.LRT);
-    const { token, dapp } = lrtsData[currentIndex];
-    //TODO
-    setCardData({
-      tokenName: token.symbol as string,
-      dappName: dapp.name,
-      dappLogo: dapp.logo,
-      apr: 0,
-      tvl: 0,
-      balance: token?.balance || 0,
-    });
   };
 
   const handleShowModal = (_actionType: any) => {
@@ -105,21 +75,10 @@ const Home = () => {
           centeredSlides={true}
           initialSlide={initialSlide}
           pagination={{ clickable: true, el: '.swiper-pagination' }}
-          // navigation={navigation}
-          // navigation={{
-          //   nextEl: '.next',
-          // }}
-          // modules={modules}
           slidesPerView={4}
           className="mySwiper"
           slideToClickedSlide={true}
           onSlideChange={handleSlideChange}
-          // on={{setTranslate:function(){
-          //   slide.css({'opacity': '','background': ''});slide.transform('');//清除样式
-          //   slide.transform('scale('+(1 - Math.abs(progress)/8)+')');
-          // slide.css('opacity',(1-Math.abs(progress)/6));
-          //   slide.transform('translate3d(0,'+ Math.abs(progress)*20+'px, 0)');
-          // }}}
         >
           {lrtsData.map((item: any) => (
             <SwiperSlide key={item.key}>
@@ -145,7 +104,7 @@ const Home = () => {
 
       <Gems data={lrtsData[currentIndex].lrtTokens} onClick={handleClickGem} />
 
-      <TabCard type={cardType} data={cardData} handleStake={handleShowModal} />
+      <TabCard data={cardData} curLrt={curLrt} handleStake={handleShowModal} />
 
       {isShowNpc ? <NpcDialog onClose={() => setIsShowNpc(false)} /> : null}
 
