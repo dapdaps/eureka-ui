a91c9181aca9d164cbb8e89f909f7db43aa4f04d
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2024-07-17 12:04:20 +0800

    fix: fix bugs

--- src/views/lrts/components/modal/stake/modules/puffer.tsx
+++ src/views/lrts/components/modal/stake/modules/puffer.tsx
@@ -2,19 +2,23 @@ import usePuffer from '../hooks/usePuffer';
 import BaseComponent from '../components/base-component';
 import { memo, useState } from 'react';
 
-<<<<<<< HEAD
 function Karak({ token0, token1, setShow, actionType, handleChangeActionType }: any) {
-=======
-function Karak({ token0, token1, setShow, dapp }: any) {
-  const [actionType, setActionType] = useState('stake');
->>>>>>> c40ae9eacd5b1f40eb3f818e0f64742d89ec5ac7
-  const { data, inAmount, outAmount, inToken, outToken, isInSufficient, isLoading, handleAmountChange, handleStake } =
-    usePuffer({
-      token0,
-      token1,
-      actionType,
-      dapp,
-    });
+  const {
+    data,
+    inAmount,
+    outAmount,
+    inToken,
+    outToken,
+    isInSufficient,
+    isLoading,
+    handleAmountChange,
+    handleStake
+  } = usePuffer({
+    token0,
+    token1,
+    actionType,
+    dapp,
+  });
 
   return (
     <BaseComponent
