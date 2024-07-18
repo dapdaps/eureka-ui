241ae0571b36f4503ba8c2c617234e9cb82ae109
Ethan <starjs@qq.com>
2024-07-18 16:15:01 +0800

    feat: empty status for table

--- src/views/lrts/portfolio.tsx
+++ src/views/lrts/portfolio.tsx
@@ -13,7 +13,7 @@ import { formatThousandsSeparator } from '@/utils/format-number';
 import { unifyNumber } from '@/utils/format-number';
 import useTokens from '@/views/lrts/hooks/useTokens';
 
-import { CustomTable, History, PolygonBtn, Tabs } from './components';
+import { CustomTable, History, PolygonBtn, Tabs, UnstakeTable } from './components';
 import AddTokenModal from './components/modal/add-token';
 import SwapModal from './components/modal/swap';
 import useAllTokensBalance from './hooks/useAllTokensBalance';
@@ -65,6 +65,8 @@ const Portfolio: FC<IProps> = (props) => {
     key: index,
     apr: item.apr,
   }));
+  // .filter((item: any) => balances[item.address] > 0);
+
   const lrtAssets = lrtsData
     .map((item: any) => item.lrtTokens)
     .flat()
@@ -117,6 +119,7 @@ const Portfolio: FC<IProps> = (props) => {
           <div className="title">LST Assets</div>
           <CustomTable
             dataSource={lstAssets}
+            emptyTips="No LST assets found in your wallet..."
             columns={[
               {
                 title: 'Assets',
@@ -193,6 +196,7 @@ const Portfolio: FC<IProps> = (props) => {
           </div>
           <CustomTable
             dataSource={lrtAssets}
+            emptyTips="No LRT assets found in your wallet..."
             columns={[
               {
                 title: 'Assets',
@@ -267,7 +271,7 @@ const Portfolio: FC<IProps> = (props) => {
         </AssetTab>
       ),
     },
-    { label: TabType.Unstake, key: 'item-2', children: <History /> },
+    { label: TabType.Unstake, key: 'item-2', children: <UnstakeTable /> },
     {
       label: TabType.History,
       key: 'item-3',
