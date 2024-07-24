3ee6b59b7347344a0fda057b8b36aabd7ef1df57
Emily <emily@ref.finance>
2024-07-24 17:00:09 +0800

    get karak token balances.

---
+++ src/views/lrts/components/modal/stake/hooks/useKarakBalance.ts
@@ -0,0 +1,55 @@
+import { ethers } from 'ethers';
+import Big from 'big.js';
+import useAccount from '@/hooks/useAccount';
+import { useCallback, useEffect, useState } from 'react';
+
+const contracts: Record<number, any> = {
+  1: {
+    VaultSupervisor: '0x54e44DbB92dBA848ACe27F44c0CB4268981eF1CC',
+  },
+};
+
+export default function useKarakBalance(chainId?: number) {
+  const [balances, setBalances] = useState<any>({});
+  const { account, provider } = useAccount();
+
+  const queryBalances = useCallback(async () => {
+    if (!chainId || !account || !contracts[chainId]) return;
+    const Contract = new ethers.Contract(
+      contracts[chainId].VaultSupervisor,
+      [
+        {
+          inputs: [{ internalType: 'address', name: 'staker', type: 'address' }],
+          name: 'getDeposits',
+          outputs: [
+            { internalType: 'contract IVault[]', name: 'vaults', type: 'address[]' },
+            { internalType: 'contract IERC20[]', name: 'tokens', type: 'address[]' },
+            { internalType: 'uint256[]', name: 'assets', type: 'uint256[]' },
+            { internalType: 'uint256[]', name: 'shares', type: 'uint256[]' },
+          ],
+          stateMutability: 'view',
+          type: 'function',
+        },
+      ],
+      provider?.getSigner(),
+    );
+    const result = await Contract.getDeposits(account);
+    const vaults = result.vaults;
+    const shares = result.shares;
+
+    const _b: any = {};
+
+    vaults.forEach((vault: string, i: number) => {
+      _b[vault.toLowerCase()] = Big(shares[i] || 0)
+        .div(1e18)
+        .toString();
+    });
+    setBalances(_b);
+  }, [chainId, account]);
+
+  useEffect(() => {
+    queryBalances();
+  }, [account]);
+
+  return { balances, queryBalances };
+}
--- src/views/lrts/hooks/useAllTokensBalance.ts
+++ src/views/lrts/hooks/useAllTokensBalance.ts
@@ -2,23 +2,34 @@ import { useEffect, useState } from 'react';
 
 import { ethereum } from '@/config/tokens/ethereum';
 import useTokensBalance from '@/hooks/useTokensBalance';
-
+import useKarakBalance from '../components/modal/stake/hooks/useKarakBalance';
 import LSTS_DATA from '../config/data';
 
 const useAllTokensBalance = (updater?: number) => {
   const [allTokens, setAllTokens] = useState<any[]>([]);
+  const [balances, setBalances] = useState<any>({});
+  const { balances: karakBalances } = useKarakBalance(1);
 
   useEffect(() => {
     const lstTokens = LSTS_DATA.map((item) => item.token);
+
     const lrTokens = LSTS_DATA.map((item) => item.lrtTokens)
       .flat()
+      .filter((item) => {
+        return !['kmETH', 'krETH', 'ksfrxETH'].includes(item.token.symbol);
+      })
       .map((item) => item.token);
 
     const allTokens = [ethereum.eth, ...lstTokens, ...lrTokens];
     setAllTokens(allTokens);
   }, [updater]);
 
-  const { loading, balances } = useTokensBalance(allTokens);
+  const { loading, balances: tokenBalances } = useTokensBalance(allTokens);
+
+  useEffect(() => {
+    if (!Object.keys(karakBalances).length || !Object.keys(tokenBalances).length) return;
+    setBalances({ ...karakBalances, ...tokenBalances });
+  }, [karakBalances, tokenBalances]);
 
   return { loading, balances };
 };
