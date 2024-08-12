f0e5c9e79b8d3f51f7d9dcab43c98b9c6d9c531f
Yyy <snwyz1st@gmail.com>
2024-08-12 12:05:53 +0800

    fix: add remove events

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -51,17 +51,27 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
 
   const componentSrc = router.query;
 
+  const handleRouteChangeStart = () => {
+    NProgress.start();
+  };
+  const handleRouteChangeComplete = () => {
+    NProgress.done();
+  };
+  const handleRouteChangeError = () => {
+    NProgress.done();
+  };
+
   useEffect(() => {
-    router.events.on('routeChangeStart', () => {
-      NProgress.start()
-    });
-    router.events.on('routeChangeComplete', () => {
-      NProgress.done()
-    });
-    router.events.on('routeChangeError', () => {
-      NProgress.done()
-    });
-  }, [])
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+    router.events.on('routeChangeError', handleRouteChangeError);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+      router.events.off('routeChangeError', handleRouteChangeError);
+    };
+  }, [router.events]);
 
   useEffect(() => {
     // Displays the Zendesk widget only if user is signed in and on the home page
