651c5274e270503434975a3b4c8c8d58c7469db3
jimmygu <jimmyguu2024@gmail.com>
2024-08-17 15:13:41 +0800

    fix: Chronos V2 throws an error in the console after entering a value and fails to calculate the token out receive
    Closed: DAP-738

--- src/config/swap/dapps/chronos-v2.ts
+++ src/config/swap/dapps/chronos-v2.ts
@@ -24,6 +24,10 @@ const networks = {
       arbitrum['arb'],
       arbitrum['dai+'],
     ],
+    factoryAddress: "0x4Db9D624F67E00dbF8ef7AE0e0e8eE54aF1dee49",
+    routerAddress: "0xE0aBdFD837D451640CF43cB1Ec4eE87976eFbb41",
+    quoterAddress: "0x6E7f0Ca45171a4440c0CDdF3A46A8dC5D4c2d4A0",
+    uniType: "v3",
   },
 };
 
