3b63fbb4616abf56d531c3477706657d66cc6318
jimmygu <jimmyguu2024@gmail.com>
2024-08-19 11:52:46 +0800

    fix: Optimize the animations on the profile page

--- src/components/bouncing-medal/index.tsx
+++ src/components/bouncing-medal/index.tsx
@@ -5,7 +5,10 @@ import {
   Composite,
   Runner,
   Mouse,
-  MouseConstraint, Events, Body,
+  MouseConstraint,
+  Events,
+  Body,
+  World,
 } from 'matter-js';
 import React, { useEffect, useRef } from 'react';
 import { styled } from 'styled-components';
@@ -16,7 +19,7 @@ const BouncingMedal = (props: BouncingMedalsProps) => {
   const DPR = window.devicePixelRatio || 2;
   const canvasRef = useRef<any>();
 
-  const create = async () => {
+  useEffect(() => {
     if (!canvasRef.current) return;
     canvasRef.current.innerHTML = '';
     const engine = Engine.create();
@@ -31,78 +34,88 @@ const BouncingMedal = (props: BouncingMedalsProps) => {
         background: 'unset',
       },
     });
-    const medalImages = medals.map((it) => loadMedal(it.icon));
-    const images: string[] = await Promise.all(medalImages);
-    const medalList = medals.map((m, idx) => Bodies.rectangle(m.x, m.y, m.width, m.height, {
-      restitution: 0.8,
-      density: m.density || 0.001,
-      render: {
-        fillStyle: '',
-        strokeStyle: '',
-        lineWidth: 0,
-        sprite: {
-          texture: images[idx],
-          xScale: 1 / DPR,
-          yScale: 1 / DPR,
-        },
-      },
-    }));
-    const borders = [
-      // top
-      Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true, render: { visible: false } }),
-      // bot
-      Bodies.rectangle(width / 2, height, width, 2, { isStatic: true, render: { visible: false } }),
-      // left
-      Bodies.rectangle(0, height / 2, 2, height, { isStatic: true, render: { visible: false } }),
-      // right
-      Bodies.rectangle(width, height / 2, 2, height, { isStatic: true, render: { visible: false } }),
-    ];
-    // const mouse = Mouse.create(render.canvas);
-    // const mouseConstraint = MouseConstraint.create(engine, {
-    //   mouse: mouse,
-    //   constraint: {
-    //     stiffness: 0.2,
-    //     render: {
-    //       visible: false,
-    //     },
-    //   },
-    // });
-    Composite.add(engine.world, [
-      ...medalList,
-      ...borders,
-      // mouseConstraint
-    ]);
 
-    // const offset = 20;
-    // Events.on(mouseConstraint, 'mousemove', function(event) {
-    //   for (const medal of medalList) {
-    //     console.log(medal);
-    //     if (medal.position.x < offset) {
-    //       Body.setPosition(medal, { x: offset, y: medal.position.y });
-    //     }
-    //     if (medal.position.x > width - offset) {
-    //       Body.setPosition(medal, { x: width - offset, y: medal.position.y });
-    //     }
-    //     if (medal.position.y < offset) {
-    //       Body.setPosition(medal, { y: offset, x: medal.position.x });
-    //     }
-    //     if (medal.position.y > height - offset) {
-    //       Body.setPosition(medal, { y: height, x: medal.position.x });
-    //     }
-    //   }
-    // });
+    const create = async () => {
+      const medalImages = medals.map((it) => loadMedal(it.icon));
+      const images: any = await Promise.all(medalImages);
+      const medalList = medals.map((m, idx) => {
+        if (!images[idx]) return null;
+        const widthScale = m.width / images[idx].width;
+        const _height = m.width / (images[idx].width / images[idx].height);
+        return Bodies.rectangle(m.x, m.y, m.width, m.height ?? _height, {
+          restitution: 0.8,
+          density: m.density || 0.001,
+          render: {
+            fillStyle: '',
+            strokeStyle: '',
+            lineWidth: 0,
+            sprite: {
+              texture: images[idx].src,
+              xScale: widthScale,
+              yScale: (images[idx].height * widthScale) / images[idx].height,
+            },
+          },
+        });
+      });
+      const borders = [
+        // top
+        Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true, render: { visible: false } }),
+        // bot
+        Bodies.rectangle(width / 2, height, width, 2, { isStatic: true, render: { visible: false } }),
+        // left
+        Bodies.rectangle(0, height / 2, 2, height, { isStatic: true, render: { visible: false } }),
+        // right
+        Bodies.rectangle(width, height / 2, 2, height, { isStatic: true, render: { visible: false } }),
+      ];
+      // const mouse = Mouse.create(render.canvas);
+      // const mouseConstraint = MouseConstraint.create(engine, {
+      //   mouse: mouse,
+      //   constraint: {
+      //     stiffness: 0.2,
+      //     render: {
+      //       visible: false,
+      //     },
+      //   },
+      // });
+      Composite.add(engine.world, [
+        ...medalList.filter((m) => !!m) as Body[],
+        ...borders,
+        // mouseConstraint
+      ]);
 
-    Render.run(render);
-    const runner = Runner.create();
-    Runner.run(runner, engine);
-  };
+      // const offset = 20;
+      // Events.on(mouseConstraint, 'mousemove', function(event) {
+      //   for (const medal of medalList) {
+      //     console.log(medal);
+      //     if (medal.position.x < offset) {
+      //       Body.setPosition(medal, { x: offset, y: medal.position.y });
+      //     }
+      //     if (medal.position.x > width - offset) {
+      //       Body.setPosition(medal, { x: width - offset, y: medal.position.y });
+      //     }
+      //     if (medal.position.y < offset) {
+      //       Body.setPosition(medal, { y: offset, x: medal.position.x });
+      //     }
+      //     if (medal.position.y > height - offset) {
+      //       Body.setPosition(medal, { y: height, x: medal.position.x });
+      //     }
+      //   }
+      // });
 
-  useEffect(() => {
+      Render.run(render);
+      const runner = Runner.create();
+      Runner.run(runner, engine);
+    };
     const timer = setTimeout(create, delay);
+
     return () => {
       clearTimeout(timer);
+      Render.stop(render);
+      World.clear(engine.world, true);
+      Engine.clear(engine);
+      render.canvas.remove();
     };
-  }, [medals]);
+  }, [JSON.stringify(medals.map((m) => ({ key: m.key, icon: m.icon })))]);
 
   return (
     <StyledCanvas
@@ -127,15 +140,20 @@ const StyledCanvas = styled.div`
   }
 `;
 
-const loadMedal = (src: string): Promise<string> => {
+const loadMedal = (src: string): Promise<{ src: string; width: number; height: number; } | boolean> => {
   return new Promise((resolve) => {
     const img = new Image();
     img.src = src;
     img.onload = () => {
-      resolve(img.src);
+      resolve({
+        src: img.src,
+        width: img.width,
+        height: img.height,
+      });
     };
     img.onerror = () => {
-      resolve(src);
+      console.log('load image error: %o', src);
+      resolve(false);
     };
   });
 };
@@ -154,7 +172,7 @@ export interface BouncingMedalItem {
   key: number;
   icon: string;
   width: number;
-  height: number;
+  height?: number;
   name?: string;
   // The initial x-axis position, note that it must be within the canvas range.
   // Minimum value is: 0, maximum value is: width - medal width
