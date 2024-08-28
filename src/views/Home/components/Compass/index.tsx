2b0318c6388329ead971a9547c77fd834d6a8fae
Yyy <snwyz1st@gmail.com>
2024-08-28 19:09:34 +0800

    fix: odyssey skeleton

--- src/views/Home/components/Compass/index.tsx
+++ src/views/Home/components/Compass/index.tsx
@@ -9,13 +9,11 @@ import { Autoplay, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import CompassIcon from '@/components/Icons/Compass';
-import Loading from '@/components/Icons/Loading';
 import odyssey from '@/config/odyssey';
 import useAuthCheck from '@/hooks/useAuthCheck';
 import useToast from '@/hooks/useToast';
 import { StyledFlex, StyledFont, StyledSvg } from '@/styled/styles';
 import { extractPathFromUrl } from '@/utils/formate';
-import Tag from '@/views/Odyssey/components/Tag';
 
 import useCompassCombineAd from './hooks/useCompassCombineAd';
 import {
@@ -33,7 +31,7 @@ import {
   StyledInner,
   StyledSwiperNextButton,
   StyledSwiperPrevButton,
-  StyledSwiperWrapper,
+  StyledSwiperWrapper
 } from './styles';
 
 const AdCard = function ({ ad }: any) {
@@ -41,32 +39,33 @@ const AdCard = function ({ ad }: any) {
   return (
     <StyledCard>
       <StyledFlex gap="32px" alignItems="flex-start" style={{ width: '100%', height: '100%' }}>
-        <StyledCardBackgroundImage
-          src={ad.ad_images}
-          alt={ad.title}
-          width={720}
-          height={400}
-        />
+        <StyledCardBackgroundImage src={ad.ad_images} alt={ad.title} width={720} height={400} />
         <StyledCardMainContent>
           <div className="title">Featured</div>
           <div className="card_section">
             {ad.title.indexOf('Super') > -1 ? (
               <StyledFlex gap={ad.title.indexOf('Bridge') ? '2px' : '10px'}>
-                {
-                  ad.title.indexOf('Bridge') > -1 ? (
-                    <StyledSvg>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="34" height="52" viewBox="0 0 34 52" fill="none">
-                        <path d="M0.774696 23.7007L25.3431 0.601338C26.2923 -0.291079 27.7804 0.767292 27.2489 1.95676L19.4189 19.4798C19.0884 20.2195 19.5758 21.0664 20.3814 21.1523L32.8501 22.4806C33.8808 22.5904 34.2905 23.872 33.5146 24.5592L3.97155 50.7269C2.97277 51.6115 1.49646 50.4387 2.13232 49.2657L13.2326 28.7893C13.6344 28.0481 13.1611 27.1353 12.3238 27.0365L1.4519 25.754C0.445479 25.6353 0.0363794 24.3949 0.774696 23.7007Z" fill="#EBF479" />
-                      </svg>
-                    </StyledSvg>
-                  ) : (
-                    <StyledSvg>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="36" height="31" viewBox="0 0 36 31" fill="none">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M14.0329 0C13.6829 0 13.3472 0.139032 13.0997 0.386518L0.389346 13.0944C-0.442312 13.9259 0.146589 15.3478 1.32262 15.3478L21.0889 15.3478C21.789 15.3478 22.4604 15.0697 22.9555 14.5747L35.2792 2.25341C36.1109 1.42192 35.522 0 34.3459 0H14.0329ZM0.389361 28.3892L11.7479 17.0328H21.7629C22.4629 17.0328 23.1343 16.7548 23.6294 16.2598L24.5946 15.2948H34.3459C35.522 15.2948 36.1109 16.7167 35.2792 17.5482L22.9555 29.8695C22.4604 30.3645 21.789 30.6426 21.089 30.6426L1.32263 30.6426C0.146604 30.6426 -0.442297 29.2207 0.389361 28.3892Z" fill="#EBF479" />
-                      </svg>
-                    </StyledSvg>
-                  )
-                }
+                {ad.title.indexOf('Bridge') > -1 ? (
+                  <StyledSvg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="52" viewBox="0 0 34 52" fill="none">
+                      <path
+                        d="M0.774696 23.7007L25.3431 0.601338C26.2923 -0.291079 27.7804 0.767292 27.2489 1.95676L19.4189 19.4798C19.0884 20.2195 19.5758 21.0664 20.3814 21.1523L32.8501 22.4806C33.8808 22.5904 34.2905 23.872 33.5146 24.5592L3.97155 50.7269C2.97277 51.6115 1.49646 50.4387 2.13232 49.2657L13.2326 28.7893C13.6344 28.0481 13.1611 27.1353 12.3238 27.0365L1.4519 25.754C0.445479 25.6353 0.0363794 24.3949 0.774696 23.7007Z"
+                        fill="#EBF479"
+                      />
+                    </svg>
+                  </StyledSvg>
+                ) : (
+                  <StyledSvg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="31" viewBox="0 0 36 31" fill="none">
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M14.0329 0C13.6829 0 13.3472 0.139032 13.0997 0.386518L0.389346 13.0944C-0.442312 13.9259 0.146589 15.3478 1.32262 15.3478L21.0889 15.3478C21.789 15.3478 22.4604 15.0697 22.9555 14.5747L35.2792 2.25341C36.1109 1.42192 35.522 0 34.3459 0H14.0329ZM0.389361 28.3892L11.7479 17.0328H21.7629C22.4629 17.0328 23.1343 16.7548 23.6294 16.2598L24.5946 15.2948H34.3459C35.522 15.2948 36.1109 16.7167 35.2792 17.5482L22.9555 29.8695C22.4604 30.3645 21.789 30.6426 21.089 30.6426L1.32263 30.6426C0.146604 30.6426 -0.442297 29.2207 0.389361 28.3892Z"
+                        fill="#EBF479"
+                      />
+                    </svg>
+                  </StyledSvg>
+                )}
                 <StyleAdTitle>{ad.title}</StyleAdTitle>
               </StyledFlex>
             ) : (
@@ -74,30 +73,27 @@ const AdCard = function ({ ad }: any) {
             )}
             <div className="card-tips">{ad.description}</div>
           </div>
-          {
-            ad.btn && (
-              <div className='btns'>
-                <StyledCompassButton
-                  onClick={() => {
-                    const route = extractPathFromUrl(ad.ad_link);
-                    if (route) {
-                      router.push(route);
-                    }
-                  }}
-                  data-bp="1001-003"
-                >
-                  <div>{ad.btn}</div>
-                  <IconClickArrow />
-                </StyledCompassButton>
-              </div>
-            )
-          }
+          {ad.btn && (
+            <div className="btns">
+              <StyledCompassButton
+                onClick={() => {
+                  const route = extractPathFromUrl(ad.ad_link);
+                  if (route) {
+                    router.push(route);
+                  }
+                }}
+                data-bp="1001-003"
+              >
+                <div>{ad.btn}</div>
+                <IconClickArrow />
+              </StyledCompassButton>
+            </div>
+          )}
         </StyledCardMainContent>
       </StyledFlex>
     </StyledCard>
   );
 };
-
 
 const CompassCard = function ({ compass }: any) {
   const toast = useToast();
@@ -106,7 +102,7 @@ const CompassCard = function ({ compass }: any) {
   const handleExplore = async function () {
     if (compass.status === 'un_start') {
       toast.fail({
-        title: 'Odyssey is upcoming...',
+        title: 'Odyssey is upcoming...'
       });
       return;
     }
@@ -123,7 +119,7 @@ const CompassCard = function ({ compass }: any) {
           width={720}
           height={400}
           style={{
-            filter: compass.status === 'ended' ? 'grayscale(100%)' : 'grayscale(0%)',
+            filter: compass.status === 'ended' ? 'grayscale(100%)' : 'grayscale(0%)'
           }}
         />
         <StyledCardMainContent>
@@ -159,7 +155,7 @@ const CompassCard = function ({ compass }: any) {
               <StyledCompassButton
                 className="plain"
                 onClick={() => {
-                  router.push('/odyssey')
+                  router.push('/odyssey');
                 }}
               >
                 <div>Explore All</div>
@@ -174,8 +170,6 @@ const CompassCard = function ({ compass }: any) {
 };
 
 const Compass = () => {
-  const router = useRouter();
-  const { check } = useAuthCheck({ isNeedAk: true });
   const size: any = useSize(window.document.getElementsByTagName('body')[0]);
 
   const { loading, adList, compassList } = useCompassCombineAd();
@@ -207,7 +201,7 @@ const Compass = () => {
                 clickable: true,
                 renderBullet: (index, className) => {
                   return `<span class="${className} swiper-pagination-bullet-${index}"></span>`;
-                },
+                }
               }}
               loop={true}
             >
@@ -233,17 +227,17 @@ const Compass = () => {
                       style={
                         compass.name === 'THRUSTER TURBO SPIN'
                           ? {
-                            position: 'absolute',
-                            right: -27,
-                            top: -33,
-                            zIndex: 20,
-                          }
+                              position: 'absolute',
+                              right: -27,
+                              top: -33,
+                              zIndex: 20
+                            }
                           : {
-                            position: 'absolute',
-                            right: -34,
-                            top: 0,
-                            zIndex: 20,
-                          }
+                              position: 'absolute',
+                              right: -34,
+                              top: 0,
+                              zIndex: 20
+                            }
                       }
                     >
                       {['ended', 'un_start'].includes(compass.status) ? (
@@ -262,7 +256,7 @@ const Compass = () => {
                           top: odyssey[compass.id]?.rewardTop ? odyssey[compass.id]?.rewardTop : 45,
                           textAlign: 'center',
                           fontWeight: 900,
-                          transform: 'rotate(-15deg)',
+                          transform: 'rotate(-15deg)'
                         }}
                       >
                         {odyssey[compass.id]?.reward}
@@ -272,28 +266,25 @@ const Compass = () => {
                 </SwiperSlide>
               ))}
             </Swiper>
-            {
-              (adList?.length + compassList?.length) > 0 && (
-                <>
-                  <StyledSwiperPrevButton
-                    onClick={() => {
-                      swiperRef.current && swiperRef.current.slidePrev();
-                    }}
-                  >
-                    <IconArrow />
-                  </StyledSwiperPrevButton>
-                  <StyledSwiperNextButton
-                    onClick={() => {
-                      swiperRef.current && swiperRef.current.slideNext();
-                    }}
-                  >
-                    <IconArrow />
-                  </StyledSwiperNextButton>
-                  <div className="swiper-pagination"></div>
-                </>
-              )
-            }
-
+            {adList?.length + compassList?.length > 0 && (
+              <>
+                <StyledSwiperPrevButton
+                  onClick={() => {
+                    swiperRef.current && swiperRef.current.slidePrev();
+                  }}
+                >
+                  <IconArrow />
+                </StyledSwiperPrevButton>
+                <StyledSwiperNextButton
+                  onClick={() => {
+                    swiperRef.current && swiperRef.current.slideNext();
+                  }}
+                >
+                  <IconArrow />
+                </StyledSwiperNextButton>
+                <div className="swiper-pagination"></div>
+              </>
+            )}
           </StyledSwiperWrapper>
         </StyledInner>
       </StyledContent>
