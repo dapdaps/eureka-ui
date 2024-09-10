192e92522d11d2dabd49c9db78efd1e10b330e03
ben <benjm240@gmail.com>
2024-09-10 18:19:41 +0800

    fix all in one bridge theme

--- src/views/SuperBridge/SubmitBtn/index.tsx
+++ src/views/SuperBridge/SubmitBtn/index.tsx
@@ -1,4 +1,5 @@
 import { useSetChain } from '@web3-onboard/react';
+import { useMemo } from 'react';
 import styled from 'styled-components';
 
 import Loading from '@/components/Icons/Loading';
@@ -7,55 +8,102 @@ import useConnectWallet from '@/hooks/useConnectWallet';
 import type { Chain, Token } from '@/types';
 
 const Container = styled.div<{ disabled?: boolean }>`
-    height: 60px;
-    line-height: 60px;
-    background-color: rgba(235, 244, 121, 1);
-    border-radius: 10px;
-    text-align: center;
-    color: rgba(55, 58, 83, 1);
-    cursor: pointer;
-    font-weight: 600;
-    font-size: 18px;
-    &.disbaled {
-        opacity: .3;
-        cursor: default;
-    }
-`
+  height: 60px;
+  line-height: 60px;
+  background-color: rgba(235, 244, 121, 1);
+  border-radius: 10px;
+  text-align: center;
+  color: rgba(55, 58, 83, 1);
+  cursor: pointer;
+  font-weight: 600;
+  font-size: 18px;
+  &.disbaled {
+    opacity: 0.3;
+    cursor: default;
+  }
+`;
 
 interface Props {
-    isLoading: boolean;
-    disabled: boolean;
-    text: string;
-    fromChain: Chain | null;
-    onClick: () => void;
-    defaultText?: string;   
-    theme?: any;
+  isLoading: boolean;
+  disabled: boolean;
+  text: string;
+  fromChain: Chain | null;
+  onClick: () => void;
+  defaultText?: string;
+  theme?: any;
 }
 
 export default function SubmitBtn({
-    isLoading, disabled, onClick, text, fromChain, defaultText = 'Bridge'
-} : Props) {
-    const { onConnect } = useConnectWallet();
-    const { account, chainId, provider } = useAccount();
-    const [{ settingChain, connectedChain }, setChain] = useSetChain();
+  isLoading,
+  disabled,
+  onClick,
+  text,
+  fromChain,
+  defaultText = 'Bridge',
+  theme
+}: Props) {
+  const { onConnect } = useConnectWallet();
+  const { account, chainId, provider } = useAccount();
+  const [{ settingChain, connectedChain }, setChain] = useSetChain();
 
-    if (!account) {
-        return <Container onClick={() => { onConnect() }}>Connect Wallet</Container>
+  console.log(theme);
+
+  const style = useMemo(() => {
+    if (theme) {
+      return {
+        backgroundColor: theme.selectBgColor,
+        color: theme.textColor
+      };
     }
 
-    if (isLoading) {
-        return <Container><Loading size={16} /> {defaultText}</Container>  
-    }
+    return {};
+  }, [theme]);
 
-    if (disabled) {
-        return <Container className="disbaled">{text}</Container>
-    }
+  if (!account) {
+    return (
+      <Container
+        style={style}
+        onClick={() => {
+          onConnect();
+        }}
+      >
+        Connect Wallet
+      </Container>
+    );
+  }
 
-    if (chainId !== fromChain?.chainId) {
-        return <Container onClick={() => { 
-            setChain({ chainId: `0x${fromChain?.chainId?.toString(16)}` })
-         }}>Switch Chain</Container>
-    }
+  if (isLoading) {
+    return (
+      <Container style={style}>
+        <Loading size={16} /> {defaultText}
+      </Container>
+    );
+  }
 
-    return <Container onClick={onClick}>{ defaultText }</Container>
+  if (disabled) {
+    return (
+      <Container style={style} className="disbaled">
+        {text}
+      </Container>
+    );
+  }
+
+  if (chainId !== fromChain?.chainId) {
+    return (
+      <Container
+        style={style}
+        onClick={() => {
+          setChain({ chainId: `0x${fromChain?.chainId?.toString(16)}` });
+        }}
+      >
+        Switch Chain
+      </Container>
+    );
+  }
+
+  return (
+    <Container style={style} onClick={onClick}>
+      {defaultText}
+    </Container>
+  );
 }
