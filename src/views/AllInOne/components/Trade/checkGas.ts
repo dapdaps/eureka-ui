fc810c3a1cff56ab62a01fc5806745f9da3b9c63
Emily <emily@ref.finance>
2024-09-10 14:07:43 +0800

    fix the error when the inputcurrency or outputcurrency is empty.

--- src/views/AllInOne/components/Trade/checkGas.ts
+++ src/views/AllInOne/components/Trade/checkGas.ts
@@ -2,12 +2,13 @@ import Big from 'big.js';
 import { utils } from 'ethers';
 
 const checkGas = ({ rawBalance, gasPrice, gasLimit, value }: any) => {
+  const _gasLimit = typeof gasLimit === 'string' ? Number(gasLimit) : gasLimit;
   const _balance = Big(utils.formatEther(rawBalance)).add(value || 0);
-  const gas = Big(gasLimit).mul(gasPrice);
+  const gas = Big(_gasLimit).mul(gasPrice);
 
   return {
     isGasEnough: _balance.lt(gas),
-    gas,
+    gas
   };
 };
 
