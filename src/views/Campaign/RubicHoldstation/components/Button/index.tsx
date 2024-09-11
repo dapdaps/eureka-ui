aa44da8171039de4655f7e36e293396b0b8f2f07
jimmygu <jimmyguu2024@gmail.com>
2024-09-11 12:29:37 +0800

    feat: Add a button component to the campaign page

--- src/views/Campaign/RubicHoldstation/components/Button/index.tsx
+++ src/views/Campaign/RubicHoldstation/components/Button/index.tsx
@@ -1,9 +1,37 @@
+import Loading from '@/components/Icons/Loading';
+
 import { StyledContainer } from './styles';
 
-const Button = () => {
+const Button = (props: Props) => {
+  const { children, style, className, onClick, disabled, loading } = props;
+
+  const handleClick = (e: any) => {
+    e.stopPropagation();
+    if (disabled || loading) return;
+    onClick && onClick();
+  };
+
   return (
-    <StyledContainer></StyledContainer>
+    <StyledContainer
+      style={style}
+      className={`${className} ${disabled ? 'disabled' : ''}`}
+      disabled={disabled}
+      onClick={handleClick}
+    >
+      {loading && <Loading size={16} />}
+      {children}
+    </StyledContainer>
   );
 };
 
 export default Button;
+
+interface Props {
+  children: any;
+  style?: React.CSSProperties;
+  className?: string;
+  disabled?: boolean;
+  loading?: boolean;
+
+  onClick?(): void;
+}
