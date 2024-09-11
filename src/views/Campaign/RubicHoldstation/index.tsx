a8b3ca3e69f36de0621ae6b1d8bac8f90240b832
jimmygu <jimmyguu2024@gmail.com>
2024-09-11 12:12:38 +0800

    feat: Add campaign pages

--- src/views/Campaign/RubicHoldstation/index.tsx
+++ src/views/Campaign/RubicHoldstation/index.tsx
@@ -1,14 +1,24 @@
+import Banner from '@/views/Campaign/RubicHoldstation/sections/Banner';
+import Holdstation from '@/views/Campaign/RubicHoldstation/sections/Holdstation';
+import Medal from '@/views/Campaign/RubicHoldstation/sections/Medal';
+import Rubic from '@/views/Campaign/RubicHoldstation/sections/Rubic';
+import Tickets from '@/views/Campaign/RubicHoldstation/sections/Tickets';
 import { StyledContainer } from '@/views/Campaign/RubicHoldstation/styles';
 
 const RubicHoldstation = (props: Props) => {
   const {} = props;
 
   return (
-    <StyledContainer>RubicHoldstation</StyledContainer>
+    <StyledContainer>
+      <Banner />
+      <Tickets />
+      <Rubic />
+      <Holdstation />
+      <Medal />
+    </StyledContainer>
   );
 };
 
 export default RubicHoldstation;
 
-interface Props {
-}
+interface Props {}
