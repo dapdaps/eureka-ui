3ba8857e24f79813657caefbe7ab02a6f7eb0d63
Emily <emily@ref.finance>
2024-09-13 16:58:41 +0800

    add bex.

---
+++ src/config/all-in-one/chains/bera-bArtio.ts
@@ -0,0 +1,24 @@
+export default {
+  title: 'Berachain bArtio',
+  path: 'bera-b',
+  icon: '',
+  bgColor: '#875426',
+  bgIcon: '',
+  textColor: '#fff',
+  selectBgColor: '#875426',
+  chainId: 80084,
+  rpcUrls: ['https://bartio.beratrail.io'],
+  defaultTab: 'Swap',
+  theme: {
+    button: {
+      bg: '#875426',
+      text: '#fff'
+    }
+  },
+  menuConfig: {
+    Swap: {
+      tab: 'Swap',
+      description: 'Trade efficiently across any assets on Blast.'
+    }
+  }
+};
