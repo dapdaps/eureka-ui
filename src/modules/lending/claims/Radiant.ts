ae993c0ae2ea1dc8f8d5d40fe435e4066200bb64
jimmygu <jimmyguu2024@gmail.com>
2024-09-25 00:02:59 +0800

    fix: Radiant Claim bug fixes

--- src/modules/lending/claims/Radiant.ts
+++ src/modules/lending/claims/Radiant.ts
@@ -1,36 +1,41 @@
 import { ethers } from 'ethers';
 import { useEffect } from 'react';
 
-export default function RadiantClaim (props: any) {
+export default function RadiantClaim(props: any) {
   const CLAIM_ABI = [
     {
-      inputs: [{ internalType: "address", name: "_user", type: "address" }],
-      name: "claimAll",
+      inputs: [{ internalType: 'address', name: '_user', type: 'address' }],
+      name: 'claimAll',
       outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
+      stateMutability: 'nonpayable',
+      type: 'function'
+    }
   ];
   const { loading, market, dapp, onSuccess, onError, account, provider } = props;
 
   useEffect(() => {
     if (!loading || !dapp.incentiveController || !account) return;
 
-    const CollateralContract = new ethers.Contract(
-      dapp.incentiveController,
-      CLAIM_ABI,
-      provider.getSigner()
-    );
-    CollateralContract.claimAll(account)
-      .then((tx: any) => {
-        tx.wait().then((res: any) => {
-          onSuccess(res);
+    const CollateralContract = new ethers.Contract(dapp.incentiveController, CLAIM_ABI, provider.getSigner());
+    const createTx = (gasLimit: any = 4000000) => {
+      CollateralContract.claimAll(account, { gasLimit })
+        .then((tx: any) => {
+          tx.wait().then((res: any) => {
+            onSuccess(res);
+          });
+        })
+        .catch((err: any) => {
+          onError(err);
         });
+    };
+    CollateralContract.estimateGas
+      .claimAll(account)
+      .then((gas: any) => {
+        createTx(gas);
       })
       .catch((err: any) => {
-        onError(err);
+        console.log('RadiantClaim estimateGas failure: %o', err);
+        createTx();
       });
   }, [loading, dapp.incentiveController, account]);
-
-
 }
