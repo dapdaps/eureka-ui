95ad230e3d1aab1f01e05387e7c7f04570b247d9
Emily <emily@ref.finance>
2024-10-15 17:49:33 +0800

    add nile.

--- src/config/tokens/linea.ts
+++ src/config/tokens/linea.ts
@@ -282,5 +282,13 @@ export const linea: { [key: string]: Token } = {
     symbol: 'LINUS',
     name: 'LINUS',
     icon: '/assets/tokens/linus.png'
+  },
+  zero: {
+    chainId: CHAIN_ID,
+    address: '0x78354f8dccb269a615a7e0a24f9b0718fdc3c7a7',
+    decimals: 18,
+    symbol: 'ZERO',
+    name: 'ZeroLend',
+    icon: '/assets/tokens/zero.webp'
   }
 };
