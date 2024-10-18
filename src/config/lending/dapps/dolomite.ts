99429f36171e26dbbb6deda6860e5a1c7c2097ad
jimmygu <jimmyguu2024@gmail.com>
2024-10-18 15:48:26 +0800

    fix: change name of TraderJoeLend to LFJ lend on add action

--- src/config/lending/dapps/dolomite.ts
+++ src/config/lending/dapps/dolomite.ts
@@ -16,6 +16,46 @@ const API_HOST = 'https://subgraphapi.dolomite.io/api/public';
 const API_ID = '1301d2d1-7a9d-4be4-9e9a-061cb8611549';
 const API_VERSION = 'v0.1.2';
 
+const graphConfig = {
+  graphApi: `${API_HOST}/${API_ID}/subgraphs/dolomite-arbitrum/${API_VERSION}/gn`,
+  blockNumberApiQuery: () => ({
+    operationName: 'getLatestBlockNumber',
+    variables: {},
+    query:
+      'query getLatestBlockNumber {\n  _meta {\n    block {\n      number\n      __typename\n    }\n    __typename\n  }\n}\n'
+  }),
+  marketInfoApiQuery: ({ blockNumber }: { blockNumber: number }) => ({
+    operationName: 'allMarketInfos',
+    variables: {
+      blockNumber
+    },
+    query:
+      'query allMarketInfos($blockNumber: Int!) {\n  marketRiskInfos(block: {number_gte: $blockNumber}) {\n    id\n    token {\n      id\n      marketId\n      __typename\n    }\n    isBorrowingDisabled\n    marginPremium\n    liquidationRewardPremium\n    oracle\n    supplyMaxWei\n    __typename\n  }\n}\n'
+  }),
+  allTokensApiQuery: ({ blockNumber }: { blockNumber: number }) => ({
+    operationName: 'allTokens',
+    variables: {
+      blockNumber
+    },
+    query:
+      'query allTokens($blockNumber: Int!) {\n  tokens(\n    first: 1000\n    orderBy: symbol\n    orderDirection: asc\n    block: {number_gte: $blockNumber}\n  ) {\n    id\n    chainId\n    marketId\n    symbol\n    name\n    decimals\n    __typename\n  }\n}\n'
+  }),
+  allTotalParsApiQuery: ({ blockNumber }: { blockNumber: number }) => ({
+    operationName: 'allTotalPars',
+    variables: {
+      blockNumber
+    },
+    query:
+      'query allTotalPars($blockNumber: Int!) {\n  totalPars(block: {number_gte: $blockNumber}) {\n    id\n    borrowPar\n    supplyPar\n    __typename\n  }\n}\n'
+  }),
+  positionListApiQuery: ({ walletAddress, blockNumber }: { walletAddress: string; blockNumber: number }) => ({
+    operationName: 'borrowPositions',
+    variables: { walletAddress: walletAddress, blockNumber: blockNumber },
+    query:
+      'query borrowPositions($blockNumber: Int!, $walletAddress: String!) {\n  borrowPositions(\n    block: {number_gte: $blockNumber}\n    where: {effectiveUser: $walletAddress, status_not: "CLOSED", marginAccount_: {accountNumber_not: 0}}\n    orderBy: openTimestamp\n    orderDirection: desc\n    first: 50\n  ) {\n    id\n    marginAccount {\n      id\n      user {\n        id\n        __typename\n      }\n      accountNumber\n      lastUpdatedTimestamp\n      lastUpdatedBlockNumber\n      __typename\n    }\n    openTransaction {\n      id\n      blockNumber\n      timestamp\n      __typename\n    }\n    closeTransaction {\n      id\n      blockNumber\n      timestamp\n      __typename\n    }\n    status\n    openTimestamp\n    closeTimestamp\n    effectiveSupplyTokens {\n      id\n      symbol\n      name\n      decimals\n      marketId\n      __typename\n    }\n    effectiveBorrowTokens {\n      id\n      symbol\n      name\n      decimals\n      marketId\n      __typename\n    }\n    effectiveUser {\n      id\n      __typename\n    }\n    amounts {\n      token {\n        id\n        symbol\n        name\n        decimals\n        marketId\n        __typename\n      }\n      expirationTimestamp\n      amountPar\n      amountWei\n      __typename\n    }\n    __typename\n  }\n}\n'
+  })
+};
+
 const networks = {
   1101: {
     depositWithdrawalProxy: '0xDfB6BAA334712cBBeb26B7537f62B81C2a87B1E8',
@@ -26,19 +66,8 @@ const networks = {
     // $120 / ($100 * liquidationRatio) = ~1.043 Health Factor
     liquidationRatio: '1.25',
     interestRatesApi: '/api.dolomite.io/tokens/1101/interest-rates',
-    graphApi: `${API_HOST}/${API_ID}/subgraphs/dolomite-polygon-zkevm/${API_VERSION}/gn`,
-    blockNumberApiQuery: () => ({
-      operationName: 'getLatestBlockNumber',
-      variables: {},
-      query:
-        'query getLatestBlockNumber {\n  _meta {\n    block {\n      number\n      __typename\n    }\n    __typename\n  }\n}\n'
-    }),
-    positionListApiQuery: ({ walletAddress, blockNumber }: { walletAddress: string; blockNumber: number }) => ({
-      operationName: 'borrowPositions',
-      variables: { walletAddress: walletAddress, blockNumber: blockNumber },
-      query:
-        'query borrowPositions($blockNumber: Int!, $walletAddress: String!) {\n  borrowPositions(\n    block: {number_gte: $blockNumber}\n    where: {effectiveUser: $walletAddress, status_not: "CLOSED", marginAccount_: {accountNumber_not: 0}}\n    orderBy: openTimestamp\n    orderDirection: desc\n    first: 50\n  ) {\n    id\n    marginAccount {\n      id\n      user {\n        id\n        __typename\n      }\n      accountNumber\n      lastUpdatedTimestamp\n      lastUpdatedBlockNumber\n      __typename\n    }\n    openTransaction {\n      id\n      blockNumber\n      timestamp\n      __typename\n    }\n    closeTransaction {\n      id\n      blockNumber\n      timestamp\n      __typename\n    }\n    status\n    openTimestamp\n    closeTimestamp\n    effectiveSupplyTokens {\n      id\n      symbol\n      name\n      decimals\n      marketId\n      __typename\n    }\n    effectiveBorrowTokens {\n      id\n      symbol\n      name\n      decimals\n      marketId\n      __typename\n    }\n    effectiveUser {\n      id\n      __typename\n    }\n    amounts {\n      token {\n        id\n        symbol\n        name\n        decimals\n        marketId\n        __typename\n      }\n      expirationTimestamp\n      amountPar\n      amountWei\n      __typename\n    }\n    __typename\n  }\n}\n'
-    }),
+    pricesApi: '/api.dolomite.io/tokens/1101/prices',
+    ...graphConfig,
     approveMax: true,
     wrappedToken: polygonZkevm['weth'],
     markets: {
@@ -87,19 +116,8 @@ const networks = {
     // $120 / ($100 * liquidationRatio) = ~1.043 Health Factor
     liquidationRatio: '1.25',
     interestRatesApi: '/api.dolomite.io/tokens/80084/interest-rates',
-    graphApi: `${API_HOST}/${API_ID}/subgraphs/dolomite-berachain/${API_VERSION}/gn`,
-    blockNumberApiQuery: () => ({
-      operationName: 'getLatestBlockNumber',
-      variables: {},
-      query:
-        'query getLatestBlockNumber {\n  _meta {\n    block {\n      number\n      __typename\n    }\n    __typename\n  }\n}\n'
-    }),
-    positionListApiQuery: ({ walletAddress, blockNumber }: { walletAddress: string; blockNumber: number }) => ({
-      operationName: 'borrowPositions',
-      variables: { walletAddress: walletAddress, blockNumber: blockNumber },
-      query:
-        'query borrowPositions($blockNumber: Int!, $walletAddress: String!) {\n  borrowPositions(\n    block: {number_gte: $blockNumber}\n    where: {effectiveUser: $walletAddress, status_not: "CLOSED", marginAccount_: {accountNumber_not: 0}}\n    orderBy: openTimestamp\n    orderDirection: desc\n    first: 50\n  ) {\n    id\n    marginAccount {\n      id\n      user {\n        id\n        __typename\n      }\n      accountNumber\n      lastUpdatedTimestamp\n      lastUpdatedBlockNumber\n      __typename\n    }\n    openTransaction {\n      id\n      blockNumber\n      timestamp\n      __typename\n    }\n    closeTransaction {\n      id\n      blockNumber\n      timestamp\n      __typename\n    }\n    status\n    openTimestamp\n    closeTimestamp\n    effectiveSupplyTokens {\n      id\n      symbol\n      name\n      decimals\n      marketId\n      __typename\n    }\n    effectiveBorrowTokens {\n      id\n      symbol\n      name\n      decimals\n      marketId\n      __typename\n    }\n    effectiveUser {\n      id\n      __typename\n    }\n    amounts {\n      token {\n        id\n        symbol\n        name\n        decimals\n        marketId\n        __typename\n      }\n      expirationTimestamp\n      amountPar\n      amountWei\n      __typename\n    }\n    __typename\n  }\n}\n'
-    }),
+    pricesApi: '/api.dolomite.io/tokens/80084/prices',
+    ...graphConfig,
     approveMax: true,
     wrappedToken: beraB['wbera'],
     markets: {
@@ -136,43 +154,7 @@ const networks = {
     liquidationRatio: '1.25',
     interestRatesApi: '/api.dolomite.io/tokens/42161/interest-rates',
     pricesApi: '/api.dolomite.io/tokens/42161/prices',
-    graphApi: `${API_HOST}/${API_ID}/subgraphs/dolomite-arbitrum/${API_VERSION}/gn`,
-    blockNumberApiQuery: () => ({
-      operationName: 'getLatestBlockNumber',
-      variables: {},
-      query:
-        'query getLatestBlockNumber {\n  _meta {\n    block {\n      number\n      __typename\n    }\n    __typename\n  }\n}\n'
-    }),
-    marketInfoApiQuery: ({ blockNumber }: { blockNumber: number }) => ({
-      operationName: 'allMarketInfos',
-      variables: {
-        blockNumber
-      },
-      query:
-        'query allMarketInfos($blockNumber: Int!) {\n  marketRiskInfos(block: {number_gte: $blockNumber}) {\n    id\n    token {\n      id\n      marketId\n      __typename\n    }\n    isBorrowingDisabled\n    marginPremium\n    liquidationRewardPremium\n    oracle\n    supplyMaxWei\n    __typename\n  }\n}\n'
-    }),
-    allTokensApiQuery: ({ blockNumber }: { blockNumber: number }) => ({
-      operationName: 'allTokens',
-      variables: {
-        blockNumber
-      },
-      query:
-        'query allTokens($blockNumber: Int!) {\n  tokens(\n    first: 1000\n    orderBy: symbol\n    orderDirection: asc\n    block: {number_gte: $blockNumber}\n  ) {\n    id\n    chainId\n    marketId\n    symbol\n    name\n    decimals\n    __typename\n  }\n}\n'
-    }),
-    allTotalParsApiQuery: ({ blockNumber }: { blockNumber: number }) => ({
-      operationName: 'allTotalPars',
-      variables: {
-        blockNumber
-      },
-      query:
-        'query allTotalPars($blockNumber: Int!) {\n  totalPars(block: {number_gte: $blockNumber}) {\n    id\n    borrowPar\n    supplyPar\n    __typename\n  }\n}\n'
-    }),
-    positionListApiQuery: ({ walletAddress, blockNumber }: { walletAddress: string; blockNumber: number }) => ({
-      operationName: 'borrowPositions',
-      variables: { walletAddress: walletAddress, blockNumber: blockNumber },
-      query:
-        'query borrowPositions($blockNumber: Int!, $walletAddress: String!) {\n  borrowPositions(\n    block: {number_gte: $blockNumber}\n    where: {effectiveUser: $walletAddress, status_not: "CLOSED", marginAccount_: {accountNumber_not: 0}}\n    orderBy: openTimestamp\n    orderDirection: desc\n    first: 50\n  ) {\n    id\n    marginAccount {\n      id\n      user {\n        id\n        __typename\n      }\n      accountNumber\n      lastUpdatedTimestamp\n      lastUpdatedBlockNumber\n      __typename\n    }\n    openTransaction {\n      id\n      blockNumber\n      timestamp\n      __typename\n    }\n    closeTransaction {\n      id\n      blockNumber\n      timestamp\n      __typename\n    }\n    status\n    openTimestamp\n    closeTimestamp\n    effectiveSupplyTokens {\n      id\n      symbol\n      name\n      decimals\n      marketId\n      __typename\n    }\n    effectiveBorrowTokens {\n      id\n      symbol\n      name\n      decimals\n      marketId\n      __typename\n    }\n    effectiveUser {\n      id\n      __typename\n    }\n    amounts {\n      token {\n        id\n        symbol\n        name\n        decimals\n        marketId\n        __typename\n      }\n      expirationTimestamp\n      amountPar\n      amountWei\n      __typename\n    }\n    __typename\n  }\n}\n'
-    }),
+    ...graphConfig,
     approveMax: true,
     wrappedToken: arbitrum['weth'],
     markets: {
