a9e2b9b7976b988e693a9263df0eae5effb60a41
ben <benjm240@gmail.com>
2024-10-23 11:46:32 +0800

    change bridge time

--- src/components/BridgeX/Utils.ts
+++ src/components/BridgeX/Utils.ts
@@ -118,31 +118,36 @@ export function isNumeric(value: any): boolean {
 const timeReg = /^\d+\w+$/;
 
 export function timeFormate(value: number | string) {
-  if (timeReg.exec(value.toString())) {
-    return value;
-  }
-
   if (!value) {
     return '~';
   }
 
   const _value = Number(value);
+  const m = Math.floor(_value / 60);
+  const s = _value % 60;
+  const h = Math.floor(m / 60);
 
-  const h = Math.floor(_value / 60);
-  const m = _value % 60;
-
-  if (h >= 1) {
-    if (h > 24) {
-      const d = Math.floor(h / 24);
-      return `~${h}h${m}min`;
-    }
+  if (h > 0) {
     if (m > 0) {
       return `~${h}h${m}min`;
     }
+
+    if (m > 0 && s > 0) {
+      return `~${h}h${m}min${s}s`;
+    }
+
     return `~${h}h`;
   }
 
-  return `~${m}min`;
+  if (m > 0) {
+    if (s > 0) {
+      return `~${m}min${s}s`;
+    }
+
+    return `~${m}min`;
+  }
+
+  return `~${value}s`;
 }
 
 export const tokenSelector = {
