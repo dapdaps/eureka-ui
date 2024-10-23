1fc841e711393537a758a9d9c192faa0d17f19ff
jimmygu <jimmyguu2024@gmail.com>
2024-10-23 18:01:49 +0800

    fix: All-in-One Blast Lending bug fixes

--- src/config/all-in-one/chains/blast.ts
+++ src/config/all-in-one/chains/blast.ts
@@ -1,3 +1,32 @@
+import styled from 'styled-components';
+
+const Theme = styled.div`
+  --button-text-color: #000;
+  --button-color: #fdfe03;
+  --primary-color: #fdfe03;
+  --border-color: #32496a;
+  --supply-bg-color: rgba(217, 159, 71, 0.2);
+  --borrow-bg-color: rgba(217, 71, 124, 0.2);
+  --supply-color: #fdfe03;
+  --borrow-color: #d9477c;
+  --withdraw-bg-color: rgba(243, 186, 47, 0.2);
+  --withdraw-bg-hover-color: #fdfe03;
+  --withdraw-border-color: #fdfe03;
+  --repay-bg-color: rgba(217, 71, 124, 0.2);
+  --repay-bg-hover-color: #d9477c;
+  --repay-border-color: #d9477c;
+  --switch-color: #fdfe03;
+  --switch-border-color: #32496a;
+  --secondary-border-color: #32496a;
+  --yours-table-title: #ffffff;
+  --claim-bg-hover-color: #fdfe03;
+  --claim-bg-color: rgba(243, 186, 47, 0.2);
+  --claim-border-color: #fdfe03;
+  --withdraw-color: #fff;
+  --replay-color: #fff;
+  --claim-color: #fff;
+`;
+
 export default {
   title: 'Blast',
   path: 'blast',
@@ -34,7 +63,8 @@ export default {
     Lending: {
       tab: 'Lending',
       path: 'bluebiu.near/widget/Blast.Lending',
-      description: 'Maximize asset utilization across Blast markets'
+      description: 'Maximize asset utilization across Blast markets',
+      Theme
     }
   }
 };
