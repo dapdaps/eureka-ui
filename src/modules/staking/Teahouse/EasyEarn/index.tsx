e94b59b2a9d6efbdecaa97e9a3b165b9c864ff18
jimmygu <jimmyguu2024@gmail.com>
2024-10-29 11:16:41 +0800

    fix: bugs to teahouse

--- src/modules/staking/Teahouse/EasyEarn/index.tsx
+++ src/modules/staking/Teahouse/EasyEarn/index.tsx
@@ -12,6 +12,7 @@ import useSwitchChain from '@/hooks/useSwitchChain';
 import Chains from '@/modules/staking/Teahouse/EasyEarn/Chains';
 import DepositModal from '@/modules/staking/Teahouse/EasyEarn/Deposit';
 import WithdrawModal from '@/modules/staking/Teahouse/EasyEarn/Withdraw';
+import { usePriceStore } from '@/stores/price';
 import { formateValueWithThousandSeparatorAndFont } from '@/utils/formate';
 import { get } from '@/utils/http';
 import { getUTCTime } from '@/utils/utc';
@@ -35,9 +36,10 @@ const APR_LIST: any = {
 };
 
 const TeahouseEasyEarn = (props: any) => {
-  const { localConfig, prices } = props;
+  const { localConfig } = props;
   const dexConfig = { ...localConfig.basic, networks: localConfig.networks };
 
+  const prices = usePriceStore((store) => store.price);
   const { account, provider, chainId } = useAccount();
   const { switching, switchChain } = useSwitchChain();
 
