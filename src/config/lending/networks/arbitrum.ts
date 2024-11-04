284e4a507002e1cb47dcd50d75995b4964ccae7c
Emily <emily@ref.finance>
2024-11-04 14:48:03 +0800

    add compound v3 to all in one on base, arb, op and polygon chains.

--- src/config/lending/networks/arbitrum.ts
+++ src/config/lending/networks/arbitrum.ts
@@ -1,6 +1,7 @@
 import multicall from '@/config/contract/multicall';
 import weth from '@/config/contract/weth';
 
+import compoundv3 from '../dapps/compoundv3';
 import cream from '../dapps/cream';
 import filda from '../dapps/filda';
 import granaryFinance from '../dapps/granary-finance';
@@ -53,6 +54,10 @@ export default {
     'Lodestar V1': {
       ...loadestar.basic,
       ...loadestar.networks[CHAIN_ID]
+    },
+    'Compound V3': {
+      ...compoundv3.basic,
+      ...compoundv3.networks[CHAIN_ID]
     }
   }
 };
