4f8b53aeefa6f9e74ab37ca1b0b3f0c26eeb40a8
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2024-11-04 10:28:54 +0800

    fix: init add all in one

--- src/config/lending/networks/mantle.ts
+++ src/config/lending/networks/mantle.ts
@@ -1,6 +1,7 @@
 import multicall from '@/config/contract/multicall';
 import weth from '@/config/contract/weth';
 
+import initCapital from '../dapps/init-capital';
 import lendle from '../dapps/lendle';
 
 const CHAIN_ID = 5000;
@@ -15,13 +16,17 @@ export default {
     noAccountTips: `${CHAIN_NAME} Lending Collection`,
     wrongNetworkTips: `To proceed, kindly switch to ${CHAIN_NAME} Chain.`,
     chainId: CHAIN_ID,
-    chainName: CHAIN_NAME,
+    chainName: CHAIN_NAME
   },
   defaultDapp: 'Lendle',
   dapps: {
     Lendle: {
       ...lendle.basic,
-      ...lendle.networks[CHAIN_ID],
+      ...lendle.networks[CHAIN_ID]
     },
-  },
+    InitCapital: {
+      ...initCapital.basic,
+      ...initCapital.networks[CHAIN_ID]
+    }
+  }
 };
