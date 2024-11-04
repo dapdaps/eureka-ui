0836b4dedd3be5cd21e2902e6ccbb33bbae83ddb
Emily <emily@ref.finance>
2024-11-04 15:40:51 +0800

    add minterest to all in one on mantle.

--- src/config/lending/networks/mantle.ts
+++ src/config/lending/networks/mantle.ts
@@ -2,6 +2,7 @@ import multicall from '@/config/contract/multicall';
 import weth from '@/config/contract/weth';
 
 import lendle from '../dapps/lendle';
+import minterest from '../dapps/minterest';
 
 const CHAIN_ID = 5000;
 const CHAIN_NAME = 'Mantle';
@@ -15,13 +16,17 @@ export default {
     noAccountTips: `${CHAIN_NAME} Lending Collection`,
     wrongNetworkTips: `To proceed, kindly switch to ${CHAIN_NAME} Chain.`,
     chainId: CHAIN_ID,
-    chainName: CHAIN_NAME,
+    chainName: CHAIN_NAME
   },
   defaultDapp: 'Lendle',
   dapps: {
     Lendle: {
       ...lendle.basic,
-      ...lendle.networks[CHAIN_ID],
+      ...lendle.networks[CHAIN_ID]
     },
-  },
+    Minterest: {
+      ...minterest.basic,
+      ...minterest.networks[CHAIN_ID]
+    }
+  }
 };
