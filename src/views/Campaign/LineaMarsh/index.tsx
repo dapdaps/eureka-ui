f7220bf3724fbc81da55bbebc1f88ebd1fa013a7
Yyy <snwyz1st@gmail.com>
2024-12-03 16:26:17 +0800

    feat: add campaign

---
+++ src/views/Campaign/LineaMarsh/index.tsx
@@ -0,0 +1,10 @@
+import Leaderboard from './Leaderboard';
+
+const LineaMarsh = () => {
+  return (
+    <div className="container">
+      <Leaderboard />
+    </div>
+  );
+};
+export default LineaMarsh;
