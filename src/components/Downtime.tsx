7d3bf92602367291ee1f7bb35b9c81a4b9d8e12d
jimmygu <jimmygujh@gmail.com>
2025-07-07 16:59:10 +0800

    fix: change the expected downtime

--- src/components/Downtime.tsx
+++ src/components/Downtime.tsx
@@ -140,7 +140,7 @@ const StyledExpected = styled.div`
 `;
 
 const systemMaintenanceDowntimeExpected =
-  process.env.NEXT_PUBLIC_SYSTEM_MAINTENANCE_DOWNTIME_EXPECTED || 'July 7, 2025 at 18:00 UTC';
+  process.env.NEXT_PUBLIC_SYSTEM_MAINTENANCE_DOWNTIME_EXPECTED || 'July 7, 2025 at 13:30 UTC';
 
 const Downtime = () => {
   return (
